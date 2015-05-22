885497df03982d866894494a1df160a16158b903
Zachary Huff <zach.huff.386@gmail.com>
2015-05-22 01:01:49 -0400

    Bind window events

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,22 @@
+var childProcess = require('child_process');
+var remote = require('remote');
 var $ = require('jquery');
 
 $(document).on('dblclick mousedown', '.no-select, .btn', false);
 
-$('.header .close').click(window.close);
+$('.header .close').click(function() {
+  remote.getCurrentWindow().close();
+});
+$('.header .maximize').click(function() {
+  if (remote.getCurrentWindow().isMaximized()) {
+    remote.getCurrentWindow().unmaximize();
+  } else {
+    remote.getCurrentWindow().maximize();
+  }
+});
+$('.header .minimize').click(function() {
+  remote.getCurrentWindow().minimize();
+});
 
 $('.profile .open-menu i, .profile .menu-backdrop').click(function(evt) {
   var $profile = $(evt.currentTarget).parent();
