aa8236e462779eeafdd98f4b6d92ff457b4f2326
Zachary Huff <zach.huff.386@gmail.com>
2015-05-27 21:49:39 -0400

    Add vars to profile

--- www/js/profile.js
+++ www/js/profile.js
@@ -2,8 +2,26 @@ var childProcess = require('child_process');
 
 var Profile = function Profile(path) {
   this.path = path;
+  this.confPath = path + '.conf';
+  this.ovpnPath = path + '.ovpn';
+  this.logPath = path + '.log';
   this.state = 'disconnected';
   this.proc = null;
+  this.data = null;
+  this.name = null;
+  this.org_id = null;
+  this.organization = null;
+  this.server_id = null;
+  this.server = null;
+  this.user_id = null;
+  this.user = null;
+  this.autostart = false;
+  this.syncHosts = [];
+  this.syncHash = null;
+  this.syncSecret = null;
+  this.syncToken = null;
+
+  this.load();
 };
 
 Profile.prototype.connect = function() {
