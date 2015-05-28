33d0b0dcb41ea94bcfc37e18be595051a03ab5ab
Zachary Huff <zach.huff.386@gmail.com>
2015-05-27 21:56:40 -0400

    Add load to profile

--- www/js/profile.js
+++ www/js/profile.js
@@ -24,6 +24,25 @@ var Profile = function Profile(path) {
   this.load();
 };
 
+Profile.prototype.load = function() {
+  fs.readFile(this.confPath, function(err, data) {
+    var confData = JSON.parse(data);
+
+    this.name = confData.name || null;
+    this.org_id = confData.org_id || null;
+    this.organization = confData.organization || null;
+    this.server_id = confData.server_id || null;
+    this.server = confData.server || null;
+    this.user_id = confData.user_id || null;
+    this.user = confData.user || null;
+    this.autostart = confData.autostart || null;
+    this.syncHosts = confData.syncHosts || [];
+    this.syncHash = confData.syncHash || null;
+    this.syncSecret = confData.syncSecret || null;
+    this.syncToken = confData.syncToken || null;
+  }.bind(this));
+};
+
 Profile.prototype.connect = function() {
   this.proc = childProcess.spawn('echo', ['-n', 'connect']);
 
