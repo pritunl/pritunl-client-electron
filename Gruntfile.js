e792804c89a4029979db54e82776250c1ac5db70
Zachary Huff <zach.huff.386@gmail.com>
2015-06-02 20:49:50 -0400

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,16 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('resources/app/package.json'),
-    windowsInstaller: {
+    pkg: grunt.file.readJSON('package.json'),
+    'create-windows-installer': {
       appDirectory: './',
       authors: 'Pritunl',
+      title: 'Pritunl',
+      description: 'Pritunl OpenVPN client',
       exe: 'pritunl.exe'
     }
   });
 
   grunt.loadNpmTasks('grunt-electron-installer');
 
-  grunt.registerTask('windows', 'windowsInstaller');
+  grunt.registerTask('windows', ['create-windows-installer']);
 };
