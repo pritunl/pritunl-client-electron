03dce04bf33da75af83de5d3a048d81282f16da5
Zachary Huff <zach.huff.386@gmail.com>
2015-06-05 14:56:19 -0400

    Fix dir in tuntap

--- tuntap/tuntap.py
+++ tuntap/tuntap.py
@@ -1,17 +1,17 @@
 import os
 import subprocess
 
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
 def add_tap_adap(self):
-    tuntap_dir = os.path.join(ROOT_DIR, 'tuntap')
-    devcon_path = os.path.join(tuntap_dir, 'devcon.exe')
+    devcon_path = os.path.join(ROOT_DIR, 'devcon.exe')
     subprocess.check_output([devcon_path, 'install',
-        'OemWin2k.inf', 'tap0902'], cwd=tuntap_dir,
+        'OemWin2k.inf', 'tap0902'], cwd=ROOT_DIR,
         creationflags=0x08000000)
     self.tap_adap_avail += 1
 
 def clear_tap_adap(self):
-    tuntap_dir = os.path.join(ROOT_DIR, 'tuntap')
-    devcon_path = os.path.join(tuntap_dir, 'devcon.exe')
+    devcon_path = os.path.join(ROOT_DIR, 'devcon.exe')
     subprocess.check_output([devcon_path, 'remove', 'tap0902'],
-        cwd=tuntap_dir, creationflags=0x08000000)
+        cwd=ROOT_DIR, creationflags=0x08000000)
     self.reset_networking()
