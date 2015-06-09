5286da5819ddf278c744fbef6dba7f23abab43e9
Zachary Huff <zach.huff.386@gmail.com>
2015-06-09 17:29:45 -0400

    Parse adap output

--- winservice/utils/utils.go
+++ winservice/utils/utils.go
@@ -5,13 +5,36 @@ import (
 	"os/exec"
 )
 
-func UpdateAdapters() (output []byte, err error) {
-	output, err = exec.Command("ipconfig", "/all").Output()
+func UpdateAdapters() (adapUsed int, adapTotal int, err error) {
+	output, err := exec.Command("ipconfig", "/all").Output()
 	if err != nil {
 		err = &CommandError{
 			errors.Wrap(err, "Update tuntap adapters failed"),
 		}
 		return
+	}
+
+	adap := false
+	adapDisc := false
+	lines := strings.Split(string(output), "\n")
+
+	for _, line := range lines {
+		line = strings.Trim(line, "\r")
+
+		if line == "" {
+			if adap {
+				adapTotal += 1
+				if !adapDisc {
+					adapUsed += 1
+				}
+			}
+			adap = false
+			adapDisc = false
+		} else if strings.Contains(line, "TAP-Windows Adapter V9") {
+			adap = true
+		} else if strings.Contains(line, "Media disconnected") {
+			adapDisc = true
+		}
 	}
 
 	return
