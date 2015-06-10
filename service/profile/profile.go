aacec3b72dda7bcb47e34c10b34ed93fd7426532
Zachary Huff <zach.huff.386@gmail.com>
2015-06-10 15:41:10 -0400

    Add profile write

--- service/profile/profile.go
+++ service/profile/profile.go
@@ -1,6 +1,27 @@
 package profile
 
+import (
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pritunl/pritunl-client-electron/service/utils"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
 type Profile struct {
 	Id   string
 	Data string
+}
+
+func (p *Profile) write() (pth string, err error) {
+	pth = filepath.Join(utils.GetTempDir(), p.Id)
+
+	err = ioutil.WriteFile(pth, []byte(p.Data), os.FileMode(0600))
+	if err != nil {
+		&WriteError{
+			errors.Wrap(err, "profile: Failed to write profile"),
+		}
+	}
+
+	return
 }
