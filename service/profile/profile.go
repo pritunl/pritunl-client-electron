109f4990ae84f8ae58a79736214ecf882601f8bd
Zachary Huff <zach.huff.386@gmail.com>
2015-06-10 17:40:48 -0400

    Add profile start

--- service/profile/profile.go
+++ service/profile/profile.go
@@ -1,10 +1,13 @@
 package profile
 
 import (
+	"bufio"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-client-electron/service/utils"
+	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
@@ -18,9 +21,75 @@ func (p *Profile) write() (pth string, err error) {
 
 	err = ioutil.WriteFile(pth, []byte(p.Data), os.FileMode(0600))
 	if err != nil {
-		&WriteError{
+		err = &WriteError{
 			errors.Wrap(err, "profile: Failed to write profile"),
 		}
+		return
+	}
+
+	return
+}
+
+func (p *Profile) Start() (err error) {
+	confPath, err := p.write()
+	if err != nil {
+		return
+	}
+
+	cmd := exec.Command(getOpenvpnPath(), "--config", confPath)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrap(err, "profile: Failed to get stdout"),
+		}
+		return
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrap(err, "profile: Failed to get stderr"),
+		}
+		return
+	}
+
+	go func() {
+		out := bufio.NewReader(stdout)
+		for {
+			line, _, err := out.ReadLine()
+			if err != nil {
+				return
+			}
+			println(string(line))
+		}
+	}()
+
+	go func() {
+		out := bufio.NewReader(stderr)
+		for {
+			line, _, err := out.ReadLine()
+			if err != nil {
+				return
+			}
+			println(string(line))
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrap(err, "profile: Failed to start openvpn"),
+		}
+		return
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		err = &ExecError{
+			errors.Wrap(err, "profile: Openvpn error occurred"),
+		}
+		return
 	}
 
 	return
