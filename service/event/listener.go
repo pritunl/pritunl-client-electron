79318e48af0501fd2e2423d0cf2aac3b32a81e44
Zachary Huff <zach.huff.386@gmail.com>
2015-06-10 23:53:06 -0400

    Add listener

---
+++ service/event/listener.go
@@ -0,0 +1,16 @@
+package event
+
+type Listener struct {
+	stream chan *Event
+}
+
+func (l *Listener) Listen() (stream chan *Event) {
+	listeners.Add(l)
+	return
+}
+
+func NewListener() (list *Listener) {
+	list = &Listener{}
+	list.stream = make(chan *Event)
+	return
+}
