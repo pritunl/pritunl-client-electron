51348e4a66ad48d040b80c54dead45d1beb47e23
Zachary Huff <zach.huff.386@gmail.com>
2015-06-10 20:32:29 -0400

    Add init to event

--- service/event/event.go
+++ service/event/event.go
@@ -1,11 +1,15 @@
 package event
 
 var (
-	Events = make(chan *Event)
+	events = make(chan *Event)
 )
 
 type Event struct {
 	Id   string      `json:"id"`
 	Type string      `json:"type"`
 	Data interface{} `json:"data"`
+}
+
+func (e *Event) Init() {
+	events <- e
 }
