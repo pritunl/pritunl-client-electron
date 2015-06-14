29ab2fc530595d496fb91fbf573df6ff14a59658
Zachary Huff <zach.huff.386@gmail.com>
2015-06-14 05:33:31 -0400

    Add start handler

--- service/handlers/start.go
+++ service/handlers/start.go
@@ -2,8 +2,25 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-client-electron/service/profile"
 )
 
 func startPost(c *gin.Context) {
+	id := c.PostForm("id")
+	data := c.PostForm("data")
 
+	go func() {
+		prfl := &profile.Profile{
+			Id:   id,
+			Data: data,
+		}
+
+		err := prfl.Start()
+		if err != nil {
+			// TODO
+			panic(err)
+		}
+	}()
+
+	c.JSON(200, nil)
 }
