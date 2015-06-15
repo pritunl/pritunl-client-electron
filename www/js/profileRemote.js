036b98d8eaac57cba4abefd137e699f627422e88
Zachary Huff <zach.huff.386@gmail.com>
2015-06-15 03:41:25 -0400

    Add text functionailty to import

--- www/js/profileRemote.js
+++ www/js/profileRemote.js
@@ -2,18 +2,31 @@ var fs = require('fs');
 var tar = require('tar');
 
 var importProfile = function(pth, callback) {
-  fs.createReadStream(pth)
-    .pipe(tar.Parse())
-    .on('entry', function (entry) {
-      var data = '';
+  var ext = pth.split('.');
+  ext = ext[ext.length - 1];
 
-      entry.on('data', function (content) {
-        data += content.toString();
-      });
-      entry.on('end', function () {
-        callback(data);
-      });
-    });
+  switch (ext) {
+    case 'ovpn':
+    case 'conf':
+      fs.readFile(pth, 'utf8', callback);
+      break;
+    case 'tar':
+      fs.createReadStream(pth)
+        .pipe(tar.Parse())
+        .on('entry', function (entry) {
+          var data = '';
+
+          entry.on('data', function (content) {
+            data += content.toString();
+          });
+          entry.on('end', function () {
+            callback(null, data);
+          });
+        });
+      break;
+    default:
+      callback('Unsupported file type', null);
+  }
 };
 
 module.exports = {
