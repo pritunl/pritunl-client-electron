26b66bd28b62e34f8e012c042b553e1731466f79
Zachary Huff <zach.huff.386@gmail.com>
2015-06-17 16:35:00 -0400

    Add build scripts

---
+++ build_osx.sh
@@ -0,0 +1,2 @@
+./node_modules/.bin/electron-packager ./ Pritunl --platform=darwin --arch=x64 --version=0.27.3 --icon=./www/img/pritunl.icns --out=build/darwin/x64
+./node_modules/.bin/electron-packager ./ Pritunl --platform=darwin --arch=ia32 --version=0.27.3 --icon=./www/img/pritunl.icns --out=build/darwin/ia32
---
+++ build_win32.cmd
@@ -0,0 +1,2 @@
+node_modules\.bin\electron-packager .\ Pritunl --platform=win32 --arch=x64 --version=0.27.3 --icon=www\img\logo.ico --out=build\win32\x64
+node_modules\.bin\electron-packager .\ Pritunl --platform=win32 --arch=ia32 --version=0.27.3 --icon=www\img\logo.ico --out=build\win32\ia32
