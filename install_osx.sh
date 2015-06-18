cf1efcbc3bd5e5c556790f06d3adbec0ee78fa03
Zachary Huff <zach.huff.386@gmail.com>
2015-06-18 05:07:44 -0400

    Add install osx

---
+++ install_osx.sh
@@ -0,0 +1,14 @@
+# Pritunl
+sudo cp -pR build/osx/Applications/Pritunl.app /Applications
+
+# Tuntap
+sudo mkdir -p /Library/Extensions
+sudo cp -pR build/osx/Library/Extensions/tap.kext /Library/Extensions/
+sudo cp -pR build/osx/Library/Extensions/tun.kext /Library/Extensions/
+sudo mkdir -p /Library/LaunchDaemons
+sudo cp build/osx/Library/LaunchDaemons/net.sf.tuntaposx.tap.plist /Library/LaunchDaemons
+sudo cp build/osx/Library/LaunchDaemons/net.sf.tuntaposx.tun.plist /Library/LaunchDaemons
+
+# Openvpn
+sudo mkdir -p /usr/share/pritunl
+sudo cp build/osx/usr/share/pritunl/openvpn /usr/share/pritunl
