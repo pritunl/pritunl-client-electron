f0cf080e48d76bf647816d9bd1937b4767862eb6
Zachary Huff <zach.huff.386@gmail.com>
2015-06-18 04:59:09 -0400

    Update build osx

--- build_osx.sh
+++ build_osx.sh
@@ -1,2 +1,14 @@
-./node_modules/.bin/electron-packager ./ Pritunl --platform=darwin --arch=x64 --version=0.27.3 --icon=./www/img/pritunl.icns --out=build/darwin/x64
-./node_modules/.bin/electron-packager ./ Pritunl --platform=darwin --arch=ia32 --version=0.27.3 --icon=./www/img/pritunl.icns --out=build/darwin/ia32
+# Pritunl
+mkdir -p build/osx/Applications
+./node_modules/.bin/electron-packager ./ Pritunl --platform=darwin --arch=x64 --version=0.27.3 --icon=./www/img/pritunl.icns --out=build/osx/Applications
+
+# Tuntap
+mkdir -p build/osx/Library/Extensions
+cp -pR tuntap_osx/tap.kext build/osx/Library/Extensions/
+cp -pR tuntap_osx/tun.kext build/osx/Library/Extensions/
+cp tuntap_osx/net.sf.tuntaposx.tap.plist build/osx/Library/LaunchDaemons/
+cp tuntap_osx/net.sf.tuntaposx.tun.plist build/osx/Library/LaunchDaemons/
+
+# Openvpn
+mkdir -p build/osx/usr/share/pritunl
+cp openvpn_osx/openvpn build/osx/usr/share/pritunl
