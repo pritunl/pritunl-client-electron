8ababc4d4bbfd9546c1fb1c4f2a6c5f08e8d70f7
Zachary Huff <zach.huff.386@gmail.com>
2015-06-19 04:17:07 -0400

    Run check watch on client exit

--- service/handlers/stop.go
+++ service/handlers/stop.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-client-electron/service/autoclean"
 	"github.com/pritunl/pritunl-client-electron/service/profile"
 )
 
@@ -9,6 +10,8 @@ func stopPost(c *gin.Context) {
 	for _, prfl := range profile.Profiles {
 		prfl.Stop()
 	}
+
+	autoclean.CheckAndCleanWatch()
 
 	c.JSON(200, nil)
 }
