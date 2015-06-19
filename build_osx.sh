018ed5e5670ceec124217ae1214e9fbf545e60e7
Zachary Huff <zach.huff.386@gmail.com>
2015-06-19 03:09:11 -0400

    Update build osx

--- build_osx.sh
+++ build_osx.sh
@@ -16,26 +16,18 @@ mkdir -p build/osx/Library/LaunchDaemons
 cp service_osx/com.pritunl.service.plist build/osx/Library/LaunchDaemons
 
 # Tuntap
-mkdir -p build/osx/Library/Extensions
-cp -pR tuntap_osx/pritunl-tap.kext build/osx/Library/Extensions/
-cp -pR tuntap_osx/pritunl-tun.kext build/osx/Library/Extensions/
-mkdir -p build/osx/Library/LaunchDaemons
-cp tuntap_osx/com.pritunl.tuntaposx.pritunl-tap.plist build/osx/Library/LaunchDaemons/
-cp tuntap_osx/com.pritunl.tuntaposx.pritunl-tun.plist build/osx/Library/LaunchDaemons/
+#mkdir -p build/osx/Library/Extensions
+#cp -pR tuntap_osx/pritunl-tap.kext build/osx/Library/Extensions/
+#cp -pR tuntap_osx/pritunl-tun.kext build/osx/Library/Extensions/
+#mkdir -p build/osx/Library/LaunchDaemons
+#cp tuntap_osx/com.pritunl.tuntaposx.pritunl-tap.plist build/osx/Library/LaunchDaemons/
+#cp tuntap_osx/com.pritunl.tuntaposx.pritunl-tun.plist build/osx/Library/LaunchDaemons/
 
 # Openvpn
 mkdir -p build/osx/usr/local/bin
 cp openvpn_osx/openvpn build/osx/usr/local/bin/pritunl-openvpn
 
 # Package
-cp install_osx.sh build/osx/install.sh
-sed -i '' 's|build/osx|.|g' build/osx/install.sh
-cp uninstall_osx.sh build/osx/uninstall.sh
-sed -i '' 's|build/osx|.|g' build/osx/uninstall.sh
 cd build
-mv osx pritunl_0.1.0-osx
-zip -r pritunl_0.1.0-osx.zip pritunl_0.1.0-osx
-mv pritunl_0.1.0-osx osx
-cd ..
-rm build/osx/install.sh
-rm build/osx/uninstall.sh
+pkgbuild --root osx --identifier com.pritunl.pkg.Pritunl --version 0.1.0 --ownership recommended --install-location / Build.pkg
+productbuild --distribution ../resources_osx/distribution.xml --version 0.1.0 Pritunl.pkg
