0860bbe7c42ac1b0c8bb442cc2866792573d69e4
Zachary Huff <zach.huff.386@gmail.com>
2015-06-20 03:02:59 -0400

    Add config

---
+++ client/www/js/config.js
@@ -0,0 +1,70 @@
+var path = require('path');
+var logger = require('./logger.js');
+
+var remote;
+try {
+  remote = require('remote');
+} catch(err) {
+}
+
+var fs;
+var app;
+if (remote) {
+  fs = remote.require('fs');
+  app = remote.require('app');
+} else {
+  fs = require('fs');
+  app = require('app');
+}
+
+var loaded;
+var waiting = [];
+var pth = path.join(app.getPath('userData'), 'pritunl.json');
+
+var settings = {
+  ubuntuClicked: 0
+};
+
+var onReady = function(callback) {
+  if (loaded) {
+    callback();
+    return;
+  }
+  waiting.push(callback);
+};
+
+var load = function() {
+  fs.readFile(pth, function(err, data) {
+    loaded = true;
+
+    try {
+      data = JSON.parse(data);
+    } catch (err) {
+      data = {};
+    }
+
+    settings.ubuntuClicked = data['ubuntu_click'];
+
+    for (var i = 0; i < waiting.length; i++) {
+      waiting[i]();
+    }
+  });
+};
+
+var save = function() {
+  fs.writeFile(pth, JSON.stringify({
+    'ubuntu_click': settings.ubuntuClicked
+  }), function(err) {
+    if (err !== null) {
+      logger.error('Failed to write conf: ' + err);
+    }
+  });
+};
+
+load();
+
+module.exports = {
+  settings: settings,
+  onReady: onReady,
+  save: save
+};
