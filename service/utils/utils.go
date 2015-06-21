1afd594e5ed05b946ad61a720be4379bba309948
Zachary Huff <zach.huff.386@gmail.com>
2015-06-20 21:10:08 -0400

    Add get log path

--- service/utils/utils.go
+++ service/utils/utils.go
@@ -36,6 +36,27 @@ func GetRootDir() (pth string) {
 	return
 }
 
+func GetLogPath() (pth string) {
+	if runtime.GOOS == "windows" {
+		pth = filepath.Join("C:", "ProgramData", "Pritunl")
+
+		err := os.MkdirAll(pth, 0700)
+		if err != nil {
+			err = &IoError{
+				errors.Wrap(err, "utils: Failed to create log directory"),
+			}
+			panic(err)
+		}
+
+		pth = filepath.Join("C:", "ProgramData", "Pritunl", "pritunl.log")
+	} else {
+		pth = filepath.Join(string(filepath.Separator),
+			"var", "log", "pritunl.log")
+	}
+
+	return
+}
+
 func GetTempDir() (pth string, err error) {
 	if runtime.GOOS == "windows" {
 		pth = filepath.Join("C:", "ProgramData", "Pritunl")
