8c2035cba93d26ee05e980df360403f3fea145cc
Zachary Huff <zach.huff.386@gmail.com>
2015-06-21 20:11:04 -0400

    Improve error init

--- client/www/js/globals.js
+++ client/www/js/globals.js
@@ -1,13 +1,19 @@
 var util = require('util');
 
-global.ErrorInit = function(stack, message, args) {
+global.ErrorInit = function(name, args) {
+  var message;
   if (args.length > 1) {
     message = util.format.apply(this, args);
+  } else {
+    message = args[0];
   }
+
   var s = message.split(': ', 2);
-  this.name = 'NetworkError';
-  this.message = message;
   this.module = s[0];
   this.formatted = s[1];
-  this.stack = stack;
+
+  this.name = name;
+  this.message = message;
+
+  this.stack = (new Error()).stack;
 };
