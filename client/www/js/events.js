8dba39d5c8653cda3b42a2fbe5a093cbf715cf0a
Zachary Huff <zach.huff.386@gmail.com>
2015-06-22 20:31:18 -0400

    Improve socket

--- client/www/js/events.js
+++ client/www/js/events.js
@@ -1,56 +1,28 @@
 var WebSocket = require('ws');
 var constants = require('./constants.js');
-var utils = require('./utils.js');
-
-var connected = false;
-var socketId = null;
 
 var connect = function(callback) {
-  if (connected) {
-    return;
-  }
-  var id = utils.uuid();
-  socketId = id;
+  var socket = new WebSocket('ws://' + constants.serviceHost + '/events');
 
-  try {
-    var socket = new WebSocket('ws://' + constants.serviceHost + '/events');
+  var reconnect = function() {
+    setTimeout(function() {
+      connect(callback);
+    }, 500);
+  };
 
-    socket.on('onerror', function() {
-      connected = false;
-    });
+  socket.on('onerror', reconnect);
+  socket.on('error', reconnect);
+  socket.on('close', reconnect);
 
-    socket.on('open', function() {
-      if (connected || id !== socketId) {
-        socket.close();
-        return;
-      }
-
-      connected = true;
-    });
-
-    socket.on('close', function() {
-      if (id === socketId) {
-        connected = false;
-      }
-    });
-
-    socket.on('message', function(data) {
-      data = JSON.parse(data);
-      callback(data);
-    });
-  } catch (e) {
-    if (id === socketId) {
-      connected = false;
-    }
-  }
+  socket.on('message', function(data) {
+    console.log(data);
+    data = JSON.parse(data);
+    callback(data);
+  });
 };
 
 var subscribe = function(callback) {
   connect(callback);
-
-  setInterval(function() {
-    connect(callback);
-  }, 30000);
 };
 
 module.exports = {
