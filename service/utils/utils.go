d88d82aa1418ff29d2d07aeef1eaad62093c99d7
Zachary Huff <zach.huff.386@gmail.com>
2015-06-26 00:44:11 -0400

    Remove update adapters

--- service/utils/utils.go
+++ service/utils/utils.go
@@ -6,10 +6,8 @@ import (
 	"encoding/hex"
 	"github.com/dropbox/godropbox/errors"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 )
 
 func Uuid() (id string) {
@@ -71,47 +69,6 @@ func GetTempDir() (pth string, err error) {
 			errors.Wrap(err, "utils: Failed to create temp directory"),
 		}
 		return
-	}
-
-	return
-}
-
-func UpdateAdapters() (adapUsed int, adapTotal int, err error) {
-	if runtime.GOOS == "linux" {
-		adapUsed = 0
-		adapTotal = 100
-		return
-	}
-
-	output, err := exec.Command("ipconfig", "/all").Output()
-	if err != nil {
-		err = &CommandError{
-			errors.Wrap(err, "utils: Update tuntap adapters failed"),
-		}
-		return
-	}
-
-	adap := false
-	adapDisc := false
-	lines := strings.Split(string(output), "\n")
-
-	for _, line := range lines {
-		line = strings.Trim(line, "\r")
-
-		if line == "" {
-			if adap {
-				adapTotal += 1
-				if !adapDisc {
-					adapUsed += 1
-				}
-			}
-			adap = false
-			adapDisc = false
-		} else if strings.Contains(line, "TAP-Windows Adapter V9") {
-			adap = true
-		} else if strings.Contains(line, "Media disconnected") {
-			adapDisc = true
-		}
 	}
 
 	return
