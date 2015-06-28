ebdbed4e90ef2a37cd611396116dc0e41a0a9364
Zachary Huff <zach.huff.386@gmail.com>
2015-06-28 15:55:01 -0400

    Remove platform from constants

--- client/www/js/constants.js
+++ client/www/js/constants.js
@@ -1,17 +1,3 @@
-var remote;
-try {
-  remote = require('remote');
-} catch(e) {
-}
-
-var platform;
-if (remote) {
-  platform = remote.process.platform;
-} else {
-  platform = process.platform;
-}
-
 module.exports = {
-  platform: platform,
   serviceHost: 'localhost:9770'
 };
