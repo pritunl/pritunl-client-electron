520620be2dc93a695b74a817b956bb6490a6c0a6
Zachary Huff <zach.huff.386@gmail.com>
2015-06-27 20:43:07 -0400

    Update build win

--- tools/build_win.cmd
+++ tools/build_win.cmd
@@ -4,4 +4,4 @@ go build -v -a
 
 cd ..\client
 npm install
-.\node_modules\.bin\electron-packager .\ pritunl --platform=win32 --arch=x64 --version=0.27.3 --icon=www\img\logo.ico --out=..\build\win --prune --asar
+.\node_modules\.bin\electron-packager .\ pritunl --platform=win32 --arch=x64 --version=0.28.3 --icon=www\img\logo.ico --out=..\build\win --prune --asar
