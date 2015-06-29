cbe162354b6ff57a45961b3b3f2db5e2ec6ff61d
Zachary Huff <zach.huff.386@gmail.com>
2015-06-29 05:23:29 -0400

    Update build command

--- tools/build_win.cmd
+++ tools/build_win.cmd
@@ -5,4 +5,4 @@ go build -v -a
 cd ..\client
 npm install
 .\node_modules\.bin\electron-rebuild
-.\node_modules\.bin\electron-packager .\ pritunl --platform=win32 --arch=x64 --version=0.28.3 --icon=www\img\logo.ico --out=..\build\win --prune --asar
+.\node_modules\.bin\electron-packager .\ pritunl --platform=win32 --arch=ia32 --version=0.28.3 --icon=www\img\logo.ico --out=..\build\win --prune --asar
