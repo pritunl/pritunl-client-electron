47eef91e8fc64caffd988edaa1c77ffb4342094f
Zachary Huff <zach.huff.386@gmail.com>
2015-07-01 03:40:22 -0400

    Remove build win

--- tools/build_win.cmd
+++
@@ -1,8 +0,0 @@
-cd service
-go get -u -f
-go build -v -a
-
-cd ..\client
-npm install
-.\node_modules\.bin\electron-rebuild
-.\node_modules\.bin\electron-packager .\ pritunl --platform=win32 --arch=ia32 --version=0.28.3 --icon=www\img\logo.ico --out=..\build\win --prune --asar
