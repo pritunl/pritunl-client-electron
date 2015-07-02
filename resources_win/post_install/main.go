5e3842a6c1c212dc70ba68eda57d6dc959660a79
Zachary Huff <zach.huff.386@gmail.com>
2015-07-02 19:42:54 -0400

    Stop service in post install

--- resources_win/post_install/main.go
+++ resources_win/post_install/main.go
@@ -17,6 +17,11 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
+	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
+		"stop", "pritunl")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 	cmd = exec.Command(filepath.Join(rootDir, "tuntap", "tuntap.exe"),
 		"uninstall")
 	cmd.Stdout = os.Stdout
