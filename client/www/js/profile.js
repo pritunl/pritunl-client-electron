6ef400bbfbf4a3ee8a2d796bb52e1d0e7d38bb7d
Zachary Huff <zach.huff.386@gmail.com>
2015-07-05 14:38:44 -0400

    Add auth error to key sync

--- client/www/js/profile.js
+++ client/www/js/profile.js
@@ -617,6 +617,8 @@ Profile.prototype.sync = function(syncHosts, callback) {
           logger.info('profile: Failed to sync conf, no subscription');
         } else if (resp.statusCode === 404) {
           logger.warning('profile: Failed to sync conf, user not found');
+        } else if (resp.statusCode === 401) {
+          logger.warning('profile: Failed to sync conf, authentication error');
         } else if (resp.statusCode === 200 && body) {
           this.updateSync(body);
         } else if (resp.statusCode !== 200) {
