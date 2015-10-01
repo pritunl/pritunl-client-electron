5e6dff10839e0f18f89ea1547ef77b258210531f
Zachary Huff <zach.huff.386@gmail.com>
2015-10-01 19:26:50 -0400

    Fix openvpn build

--- tools/build_openvpn.sh
+++ tools/build_openvpn.sh
@@ -1,3 +1,3 @@
 ./configure --enable-password-save --disable-debug --disable-silent-rules --disable-server --disable-management --disable-plugins --disable-plugin-auth-pam --disable-plugin-down-root
 sed -i '' 's|LZO_LIBS = -llzo2|LZO_LIBS = -static /usr/local/lib/liblzo2.a|g' src/openvpn/Makefile
-make LZO_LIBS=-lliblzo2.2.dylib DESTDIR="`pwd`root/" install
+make LZO_LIBS=-lliblzo2.2.dylib DESTDIR="`pwd`/root/" install
