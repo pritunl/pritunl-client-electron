33008aacfeb9c6e0fee3ec097ea05299e5ce6337
Zachary Huff <zach.huff.386@gmail.com>
2016-01-03 21:44:38 -0500

    Use password field for pin

--- client/www/templates/profile.js
+++ client/www/templates/profile.js
@@ -22,7 +22,7 @@ module.exports = `<div class="profile" flex layout horizontal>
       placeholder="Username">
     <input class="connect-pass-input" type="password" tabindex="-1"
       placeholder="Password">
-    <input class="connect-pin-input" type="text" tabindex="-1"
+    <input class="connect-pin-input" type="password" tabindex="-1"
       placeholder="Enter PIN">
     <input class="connect-otp-input" type="text" tabindex="-1"
       placeholder="Enter OTP Code">
