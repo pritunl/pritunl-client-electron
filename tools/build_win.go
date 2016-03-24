ecd2eac26ed081911066452417cbd2ff9804f21e
Zachary Huff <zach.huff.386@gmail.com>
2016-03-24 04:11:37 -0400

    Update build_win

--- tools/build_win.go
+++ tools/build_win.go
@@ -85,7 +85,7 @@ func main() {
 		"pritunl",
 		"--platform=win32",
 		"--arch=ia32",
-		"--version=0.36.7",
+		"--version=0.37.2",
 		"--icon=www\\img\\logo.ico",
 		"--out=..\\build\\win",
 		"--prune",
