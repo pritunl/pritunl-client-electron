37ab32433f6f9786314ac43aafedd5e8650c1535
Zachary Huff <zach.huff.386@gmail.com>
2016-08-14 19:06:02 -0400

    Disable tray menu

--- client/www/main.js
+++ client/www/main.js
@@ -231,25 +231,25 @@ app.on('ready', function() {
       openMainWin();
     });
 
-    var trayMenu = Menu.buildFromTemplate([
-      {
-        label: 'Settings',
-        click: function() {
-          openMainWin();
-        }
-      },
-      {
-        label: 'Exit',
-        click: function() {
-          request.post({
-            url: 'http://' + constants.serviceHost + '/stop'
-          }, function() {
-            app.quit();
-          });
-        }
-      }
-    ]);
-    tray.setContextMenu(trayMenu);
+    // var trayMenu = Menu.buildFromTemplate([
+    //   {
+    //     label: 'Settings',
+    //     click: function() {
+    //       openMainWin();
+    //     }
+    //   },
+    //   {
+    //     label: 'Exit',
+    //     click: function() {
+    //       request.post({
+    //         url: 'http://' + constants.serviceHost + '/stop'
+    //       }, function() {
+    //         app.quit();
+    //       });
+    //     }
+    //   }
+    // ]);
+    // tray.setContextMenu(trayMenu);
 
     var appMenu = Menu.buildFromTemplate([
       {
