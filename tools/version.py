362dfb95808dcc5d9d7ff5d0bd1365f196511cbe
Zachary Huff <zach.huff.386@gmail.com>
2016-08-15 23:24:39 -0400

    Add version to tools

---
+++ tools/version.py
@@ -0,0 +1,17 @@
+import datetime
+import math
+import re
+import sys
+
+cur_date = datetime.datetime.utcnow()
+
+def get_ver(version):
+    day_num = (cur_date - datetime.datetime(2013, 9, 12)).days
+    min_num = int(math.floor(((cur_date.hour * 60) + cur_date.minute) / 14.4))
+    ver = re.findall(r'\d+', version)
+    ver_str = '.'.join((ver[0], ver[1], str(day_num), str(min_num)))
+    ver_str += ''.join(re.findall('[a-z]+', version))
+
+    return ver_str
+
+print get_ver(sys.argv[1])
