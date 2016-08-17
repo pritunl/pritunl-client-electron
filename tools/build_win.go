657d4834754930d3ead19db5440c0836fd6a79d6
Zachary Huff <zach.huff.386@gmail.com>
2016-08-17 00:06:23 -0400

    Add cache clean to build win

--- tools/build_win.go
+++ tools/build_win.go
@@ -7,10 +7,18 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("git", "pull")
+	cmd := exec.Command("npm", "cache", "clean")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("git", "pull")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
 	if err != nil {
 		panic(err)
 	}
