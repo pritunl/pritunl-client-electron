37402ca4769896ad7fa29eec2af261e7ea3d2c35
Zachary Huff <zach.huff.386@gmail.com>
2016-08-16 20:47:13 -0400

    Add learn address to profile cmd

--- service/profile/profile.go
+++ service/profile/profile.go
@@ -425,6 +425,7 @@ func (p *Profile) Start(timeout bool) (err error) {
 			"--tls-verify", blockPath,
 			"--ipchange", blockPath,
 			"--route-up", blockPath,
+			"--learn-address", blockPath,
 		)
 	} else {
 		args = append(args, "--script-security", "1")
