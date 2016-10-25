0fdc69208630225b90161a8dd7199b2806b672b2
Zachary Huff <zach.huff.386@gmail.com>
2016-10-25 01:57:11 -0400

    Watch dns

--- service/watch/watch.go
+++ service/watch/watch.go
@@ -2,6 +2,9 @@ package watch
 
 import (
 	"github.com/pritunl/pritunl-client-electron/service/profile"
+	"github.com/pritunl/pritunl-client-electron/service/utils"
+	"runtime"
+	"strings"
 	"sync"
 	"time"
 )
@@ -35,7 +38,43 @@ func wakeWatch(delay time.Duration) {
 	}
 }
 
+func dnsWatch() {
+	if runtime.GOOS != "darwin" {
+		return
+	}
+
+	reset := false
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		if !profile.GetStatus() {
+			continue
+		}
+
+		openvpn, _ := utils.GetScutilKey("/Network/OpenVPN/DNS")
+		global, _ := utils.GetScutilKey("/Network/Global/DNS")
+
+		if strings.Contains(openvpn, "No such key") ||
+			strings.Contains(global, "No such key") {
+			continue
+		}
+
+		if openvpn != global {
+			if reset {
+				profile.RestartProfiles()
+				time.Sleep(60 * time.Second)
+			} else {
+				reset = true
+			}
+		} else {
+			reset = false
+		}
+	}
+}
+
 func StartWatch() {
 	go wakeWatch(10 * time.Millisecond)
 	go wakeWatch(100 * time.Millisecond)
+	go dnsWatch()
 }
