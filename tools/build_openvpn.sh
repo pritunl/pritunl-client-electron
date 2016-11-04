bc47547ba66a34990a1932f34fe7023e3f102beb
Zachary Huff <zach.huff.386@gmail.com>
2016-11-04 01:30:34 -0400

    Update build openvpn

--- tools/build_openvpn.sh
+++ tools/build_openvpn.sh
@@ -1,7 +1,2 @@
-./configure --enable-password-save --disable-debug --disable-silent-rules --disable-server --disable-management --disable-plugins --disable-plugin-auth-pam --disable-plugin-down-root
-sed -i '' 's|CPPFLAGS = |CPPFLAGS = -I/usr/local/opt/openssl/include |g' src/openvpn/Makefile
-sed -i '' 's|OPENSSL_CRYPTO_LIBS = -lcrypto|OPENSSL_CRYPTO_LIBS = -static /usr/local/opt/openssl/lib/libcrypto.a|g' src/openvpn/Makefile
-sed -i '' 's|OPENSSL_SSL_LIBS = -lssl|OPENSSL_SSL_LIBS = -static /usr/local/opt/openssl/lib/libssl.a|g' src/openvpn/Makefile
-sed -i '' 's|OPTIONAL_CRYPTO_LIBS =  -lssl -lcrypto|OPTIONAL_CRYPTO_LIBS = -static /usr/local/opt/openssl/lib/libssl.a -static /usr/local/opt/openssl/lib/libcrypto.a|g' src/openvpn/Makefile
-sed -i '' 's|LZO_LIBS = -llzo2|LZO_LIBS = -static /usr/local/lib/liblzo2.a|g' src/openvpn/Makefile
-make LZO_LIBS=-lliblzo2.2.dylib DESTDIR="`pwd`/root/" install
+CPPFLAGS="-I/usr/local/include -L/usr/local/lib -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" OPENSSL_CRYPTO_LIBS="-static /usr/local/opt/openssl/lib/libcrypto.a" OPTIONAL_CRYPTO_LIBS="" OPENSSL_SSL_LIBS="-static /usr/local/opt/openssl/lib/libssl.a" LZO_LIBS="-static /usr/local/opt/lzo/lib/liblzo2.a" OPTIONAL_LZO_LIBS="-static /usr/local/opt/lzo/lib/liblzo2.a" ./configure --disable-debug --disable-silent-rules --disable-server --disable-management --disable-plugins --disable-plugin-auth-pam --disable-plugin-down-root
+make DESTDIR="`pwd`/root/" install
