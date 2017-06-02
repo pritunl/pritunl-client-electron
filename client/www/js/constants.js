cb55967b70f702aea079c0805a6b8d16d4ee484e
Zachary Huff <zach.huff.386@gmail.com>
2017-06-02 19:31:47 -0400

    Set version

--- client/www/js/constants.js
+++ client/www/js/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   key: '',
-  version: '1.0.1356.36',
+  version: '1.0.1359.97',
   serviceHost: 'localhost:9770'
 };
--- service/constants/constants.go
+++ service/constants/constants.go
@@ -1,5 +1,5 @@
 package constants
 
 const (
-	Version = "1.0.1356.36"
+	Version = "1.0.1359.97"
 )
