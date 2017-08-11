c9f8954363a1f699c4371b0c41fe95aab2c847a0
Zachary Huff <zach.huff.386@gmail.com>
2017-08-10 22:33:26 -0400

    Update uninstall script

--- tools/uninstall_osx.sh
+++ tools/uninstall_osx.sh
@@ -4,27 +4,22 @@ sudo launchctl unload /Library/LaunchAgents/com.pritunl.client.plist
 sudo launchctl unload /Library/LaunchDaemons/com.pritunl.service.plist
 
 # Pritunl
-sudo rm -rf /var/lib/pritunl
-sudo rm -f /var/log/pritunl.log
 sudo rm -rf /Applications/Pritunl.app
+sudo rm -f /Library/LaunchAgents/com.pritunl.client.plist
+sudo rm -f /Library/LaunchDaemons/com.pritunl.service.plist
 sudo rm -f /private/var/db/receipts/com.pritunl.pkg.Pritunl.bom
 sudo rm -f /private/var/db/receipts/com.pritunl.pkg.Pritunl.plist
 
-# Service
-sudo rm -rf /usr/local/bin/pritunl-service
-
-# Service Daemon
-sudo rm -f /Library/LaunchDaemons/com.pritunl.service.plist
-
-# Tuntap
+# Old Files
+sudo rm -rf /var/lib/pritunl
+sudo rm -f /var/log/pritunl.log
 sudo kextunload -b net.sf.tuntaposx.tap || true
 sudo kextunload -b net.sf.tuntaposx.tun || true
 sudo rm -rf /Library/Extensions/tap.kext
 sudo rm -rf /Library/Extensions/tun.kext
 sudo rm -f /Library/LaunchDaemons/net.sf.tuntaposx.tap.plist
 sudo rm -f /Library/LaunchDaemons/net.sf.tuntaposx.tun.plist
-
-# Openvpn
 sudo rm -rf /usr/local/bin/pritunl-openvpn
+sudo rm -rf /usr/local/bin/pritunl-service
 
 echo "Uninstallation Successful"
