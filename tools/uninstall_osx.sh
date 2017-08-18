69bc8336a8937f275924cee094589c06ee81488d
Zachary Huff <zach.huff.386@gmail.com>
2017-08-18 06:03:30 -0400

    Update old uninstall

--- tools/uninstall_osx.sh
+++ tools/uninstall_osx.sh
@@ -1,7 +1,7 @@
 # Stop Pritunl
-kill -2 $(ps aux | grep Pritunl.app | awk '{print $2}')
-sudo launchctl unload /Library/LaunchAgents/com.pritunl.client.plist
-sudo launchctl unload /Library/LaunchDaemons/com.pritunl.service.plist
+kill -2 $(ps aux | grep Pritunl.app | awk '{print $2}') &> /dev/null || true
+sudo launchctl unload /Library/LaunchAgents/com.pritunl.client.plist &> /dev/null || true
+sudo launchctl unload /Library/LaunchDaemons/com.pritunl.service.plist &> /dev/null || true
 
 # Pritunl
 sudo rm -rf /Applications/Pritunl.app
@@ -18,8 +18,8 @@ rm -rf ~/Library/Preferences/com.electron.pritunl.plist
 # Old Files
 sudo rm -rf /var/lib/pritunl
 sudo rm -f /var/log/pritunl.log
-sudo kextunload -b net.sf.tuntaposx.tap || true
-sudo kextunload -b net.sf.tuntaposx.tun || true
+sudo kextunload -b net.sf.tuntaposx.tap &> /dev/null || true
+sudo kextunload -b net.sf.tuntaposx.tun &> /dev/null || true
 sudo rm -rf /Library/Extensions/tap.kext
 sudo rm -rf /Library/Extensions/tun.kext
 sudo rm -f /Library/LaunchDaemons/net.sf.tuntaposx.tap.plist
@@ -27,4 +27,6 @@ sudo rm -f /Library/LaunchDaemons/net.sf.tuntaposx.tun.plist
 sudo rm -rf /usr/local/bin/pritunl-openvpn
 sudo rm -rf /usr/local/bin/pritunl-service
 
+echo "###################################################"
 echo "Uninstallation Successful"
+echo "###################################################"
