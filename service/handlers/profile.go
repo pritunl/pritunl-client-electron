f493edbde2a137b3ddac0a143a91705c9139c279
Zachary Huff <zach.huff.386@gmail.com>
2018-11-08 01:14:11 -0500

    Update profile handler

--- service/handlers/profile.go
+++ service/handlers/profile.go
@@ -6,12 +6,13 @@ import (
 )
 
 type profileData struct {
-	Id        string `json:"id"`
-	Data      string `json:"data"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Reconnect bool   `json:"reconnect"`
-	Timeout   bool   `json:"timeout"`
+	Id              string `json:"id"`
+	Data            string `json:"data"`
+	Username        string `json:"username"`
+	Password        string `json:"password"`
+	ServerPublicKey string `json:"server_public_key"`
+	Reconnect       bool   `json:"reconnect"`
+	Timeout         bool   `json:"timeout"`
 }
 
 func profileGet(c *gin.Context) {
@@ -23,11 +24,12 @@ func profilePost(c *gin.Context) {
 	c.Bind(data)
 
 	prfl := &profile.Profile{
-		Id:        data.Id,
-		Data:      data.Data,
-		Username:  data.Username,
-		Password:  data.Password,
-		Reconnect: data.Reconnect,
+		Id:              data.Id,
+		Data:            data.Data,
+		Username:        data.Username,
+		Password:        data.Password,
+		ServerPublicKey: data.ServerPublicKey,
+		Reconnect:       data.Reconnect,
 	}
 	prfl.Init()
 
