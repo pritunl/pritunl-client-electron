b0e9b8c7f88b0c72da578a3dacc0deb09adf9f0b
Zachary Huff <zach.huff.386@gmail.com>
2018-11-08 02:05:15 -0500

    Add token

---
+++ service/token/token.go
@@ -0,0 +1,38 @@
+package token
+
+import (
+	"github.com/pritunl/pritunl-client-electron/service/utils"
+	"time"
+)
+
+type Token struct {
+	Profile   string    `json:"profile"`
+	Token     string    `json:"-"`
+	Timestamp time.Time `json:"timestamp"`
+	Ttl       int       `json:"ttl"`
+	Valid     bool      `json:"valid"`
+}
+
+func (t *Token) Init() (err error) {
+	token, err := utils.RandStr(64)
+	if err != nil {
+		return
+	}
+
+	t.Token = token
+	t.Timestamp = time.Now()
+	t.Valid = false
+
+	return
+}
+
+func (t *Token) Update() (err error) {
+	if time.Since(t.Timestamp) > time.Duration(t.Ttl)*time.Second {
+		err = t.Init()
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
