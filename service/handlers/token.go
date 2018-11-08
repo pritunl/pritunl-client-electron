8354a3d568760df5c31f308dee4b48d0d8f23be2
Zachary Huff <zach.huff.386@gmail.com>
2018-11-08 02:05:02 -0500

    Add token handlers

---
+++ service/handlers/token.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-client-electron/service/token"
+)
+
+type tokenData struct {
+	Profile string `json:"profile"`
+	Ttl     int    `json:"ttl"`
+}
+
+func tokenPut(c *gin.Context) {
+	data := &tokenData{}
+	c.Bind(data)
+
+	tokn, err := token.Update(data.Profile, data.Ttl)
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	c.JSON(200, tokn)
+}
+
+func tokenDelete(c *gin.Context) {
+	data := &tokenData{}
+	c.Bind(data)
+
+	token.Clear(data.Profile)
+
+	c.JSON(200, nil)
+}
