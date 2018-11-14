2dc3498f345292546682cb068e6901bed2d6a818
Zachary Huff <zach.huff.386@gmail.com>
2018-11-14 03:17:03 -0500

    Add clean to formulas

---
+++ tools/formulas/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+brew uninstall --force openvpn
+brew uninstall --force pkcs11-helper
+brew uninstall --force openssl
+brew uninstall --force lz4
+brew uninstall --force lzo
+brew uninstall --force makedepend
+brew uninstall --force pkg-config
+brew uninstall --force automake
+brew uninstall --force autoconf
+brew uninstall --force libtool
