c4112cccd843357e3d948dd2ab41a2f02c22c0fb
Zachary Huff <zach.huff.386@gmail.com>
2019-10-12 06:17:15 -0400

    Add unix path to www constants

--- client/www/js/constants.js
+++ client/www/js/constants.js
@@ -1,5 +1,7 @@
 module.exports = {
   key: '',
   version: '1.0.2207.23',
-  serviceHost: '127.0.0.1:9770'
+  serviceHost: '127.0.0.1:9770',
+  unixSocket: false,
+  unixPath: '/var/run/pritunl.sock'
 };
