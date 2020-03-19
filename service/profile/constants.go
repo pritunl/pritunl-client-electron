6f29b85768809b10b6faf67e78abc5f062df06c9
Zachary Huff <zach.huff.386@gmail.com>
2020-03-18 21:48:47 -0400

    Add constants to profile

---
+++ service/profile/constants.go
@@ -0,0 +1,33 @@
+package profile
+
+import (
+	"text/template"
+)
+
+const (
+	WgWinPath   = "C:\\Program Files\\WireGuard\\wireguard.exe"
+	Ovpn        = "ovpn"
+	Wg          = "wg"
+	wgConfTempl = `[Interface]
+Address = {{.Address}}
+PrivateKey = {{.PrivateKey}}{{if .HasDns}}
+DNS = {{.DnsServers}}{{end}}
+
+[Peer]
+PublicKey = {{.PublicKey}}
+AllowedIPs = {{.AllowedIps}}
+Endpoint = {{.Endpoint}}
+`
+)
+
+var WgConfTempl = template.Must(template.New("wg_conf").Parse(wgConfTempl))
+
+type WgConfData struct {
+	Address    string
+	PrivateKey string
+	HasDns     bool
+	DnsServers string
+	PublicKey  string
+	AllowedIps string
+	Endpoint   string
+}
