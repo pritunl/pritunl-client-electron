83fa16bb744c42c80eec37d57d6dd030f18f7955
Zachary Huff <zach.huff.386@gmail.com>
2020-03-19 06:04:49 -0400

    Add interface to network

---
+++ service/network/interface.go
@@ -0,0 +1,60 @@
+package network
+
+import (
+	"fmt"
+	"runtime"
+	"sort"
+	"sync"
+)
+
+var (
+	interfaces     = []string{}
+	interfacesLock = sync.Mutex{}
+)
+
+func InterfaceAcquire() (name string) {
+	interfacesLock.Lock()
+	defer interfacesLock.Unlock()
+
+	if len(interfaces) > 0 {
+		name, interfaces = interfaces[0], interfaces[1:]
+	}
+
+	return
+}
+
+func InterfaceRelease(name string) {
+	if name == "" {
+		return
+	}
+
+	interfacesLock.Lock()
+	defer interfacesLock.Unlock()
+
+	exists := false
+	for _, iface := range interfaces {
+		if name == iface {
+			exists = true
+			break
+		}
+	}
+
+	if !exists {
+		interfaces = append(interfaces, name)
+		sort.Strings(interfaces)
+	}
+
+	return
+}
+
+func init() {
+	if runtime.GOOS == "windows" {
+		for i := 0; i < 10; i++ {
+			interfaces = append(interfaces, fmt.Sprintf("pritunl%d", i))
+		}
+	} else {
+		for i := 0; i < 10; i++ {
+			interfaces = append(interfaces, fmt.Sprintf("wg%d", i))
+		}
+	}
+}
