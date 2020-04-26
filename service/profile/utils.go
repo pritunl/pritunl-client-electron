beb2a89a2eaf05d6a2c93dbb25ef135689422b3c
Zachary Huff <zach.huff.386@gmail.com>
2020-04-26 10:02:13 -0400

    Update macos path in profile utils

--- service/profile/utils.go
+++ service/profile/utils.go
@@ -64,7 +64,13 @@ func GetWgQuickPath() string {
 
 		break
 	case "darwin":
-		path, _ := exec.LookPath("/usr/bin/wg-quick")
+		path, _ := exec.LookPath(
+			"/Applications/Pritunl.app/Contents/Resources/wg-quick")
+		if path != "" {
+			return path
+		}
+
+		path, _ = exec.LookPath("/usr/bin/wg-quick")
 		if path != "" {
 			return path
 		}
