4bae7bfc89f3a6627b056e40ec40ee1b0c280cb8
Zachary Huff <zach.huff.386@gmail.com>
2020-05-15 18:26:56 -0400

    Fix formatting in js utils

--- client/www/js/utils.js
+++ client/www/js/utils.js
@@ -6,7 +6,8 @@ var uuid = function() {
   var id = '';
 
   for (var i = 0; i < 8; i++) {
-    id += Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    id += Math.floor((1 + Math.random()) * 0x10000).toString(
+      16).substring(1);
   }
 
   return id;
