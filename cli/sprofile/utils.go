f56d8bdf58876195cffbe5b3dfeda0949cb12e82
Zachary Huff <zach.huff.386@gmail.com>
2020-08-18 14:34:35 -0400

    Add start to cli sprofile utils

--- cli/sprofile/utils.go
+++ cli/sprofile/utils.go
@@ -12,6 +12,24 @@ import (
 	"github.com/pritunl/pritunl-client-electron/cli/service"
 )
 
+type SprofileData struct {
+	Id                 string `json:"id"`
+	Mode               string `json:"mode"`
+	OrgId              string `json:"org_id"`
+	UserId             string `json:"user_id"`
+	ServerId           string `json:"server_id"`
+	SyncToken          string `json:"sync_token"`
+	SyncSecret         string `json:"sync_secret"`
+	Data               string `json:"data"`
+	Username           string `json:"username"`
+	Password           string `json:"password"`
+	ServerPublicKey    string `json:"server_public_key"`
+	ServerBoxPublicKey string `json:"server_box_public_key"`
+	TokenTtl           int    `json:"token_ttl"`
+	Reconnect          bool   `json:"reconnect"`
+	Timeout            bool   `json:"timeout"`
+}
+
 func Match(sprflId string) (sprfl *Sprofile, err error) {
 	sprfls, err := GetAll()
 	if err != nil {
@@ -159,6 +177,84 @@ func GetAll() (sprfls []*Sprofile, err error) {
 		if sprfl != nil {
 			sprfl.Profile = prfl
 		}
+	}
+
+	return
+}
+
+func Start(sprflId, mode, password string) (err error) {
+	sprfl, err := Match(sprflId)
+	if err != nil {
+		return
+	}
+
+	if mode == "" {
+		mode = sprfl.LastMode
+		if mode == "" {
+			mode = "ovpn"
+		}
+	}
+
+	switch mode {
+	case "ovpn", "wg":
+		break
+	default:
+		err = errortypes.NotFoundError{
+			errors.New("sprofile: Invalid profile mode"),
+		}
+		return
+	}
+
+	reqUrl := service.GetAddress() + "/profile"
+
+	authKey, err := service.GetAuthKey()
+	if err != nil {
+		return
+	}
+
+	data, err := json.Marshal(&SprofileData{
+		Id:       sprfl.Id,
+		Mode:     mode,
+		Password: password,
+	})
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Json marshal error"),
+		}
+		return
+	}
+
+	body := bytes.NewBuffer(data)
+
+	req, err := http.NewRequest("POST", reqUrl, body)
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Delete request failed"),
+		}
+		return
+	}
+
+	if runtime.GOOS == "linux" {
+		req.Host = "unix"
+	}
+	req.Header.Set("Auth-Key", authKey)
+	req.Header.Set("User-Agent", "pritunl")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := service.GetClient().Do(req)
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Request failed"),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Unknown request error"),
+		}
+		return
 	}
 
 	return
