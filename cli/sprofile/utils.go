515244e6cc7f5f17bcdcca4eb883a2aa922723ef
Zachary Huff <zach.huff.386@gmail.com>
2020-08-18 13:49:43 -0400

    Add stop to cli sprofile utils

--- cli/sprofile/utils.go
+++ cli/sprofile/utils.go
@@ -1,6 +1,7 @@
 package sprofile
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"runtime"
@@ -10,6 +11,89 @@ import (
 	"github.com/pritunl/pritunl-client-electron/cli/profile"
 	"github.com/pritunl/pritunl-client-electron/cli/service"
 )
+
+func Stop(sprflId string) (err error) {
+	sprfls, err := GetAll()
+	if err != nil {
+		return
+	}
+
+	var matched *Sprofile
+	for _, sprfl := range sprfls {
+		if sprflId == sprfl.Id {
+			matched = sprfl
+		} else if len(sprflId) <= len(sprfl.Id) &&
+			sprfl.Id[:len(sprflId)] == sprflId {
+
+			if matched != nil {
+				err = errortypes.NotFoundError{
+					errors.Wrap(err, "sprofile: Profile duplicate match"),
+				}
+				return
+			}
+			matched = sprfl
+		}
+	}
+
+	if matched == nil {
+		err = errortypes.NotFoundError{
+			errors.Wrap(err, "sprofile: Profile not found"),
+		}
+		return
+	}
+
+	reqUrl := service.GetAddress() + "/profile"
+
+	authKey, err := service.GetAuthKey()
+	if err != nil {
+		return
+	}
+
+	data, err := json.Marshal(&Sprofile{
+		Id: matched.Id,
+	})
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Json marshal error"),
+		}
+		return
+	}
+
+	body := bytes.NewBuffer(data)
+
+	req, err := http.NewRequest("DELETE", reqUrl, body)
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Delete request failed"),
+		}
+		return
+	}
+
+	if runtime.GOOS == "linux" {
+		req.Host = "unix"
+	}
+	req.Header.Set("Auth-Key", authKey)
+	req.Header.Set("User-Agent", "pritunl")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := service.GetClient().Do(req)
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Request failed"),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "sprofile: Unknown request error"),
+		}
+		return
+	}
+
+	return
+}
 
 func GetAll() (sprfls []*Sprofile, err error) {
 	reqUrl := service.GetAddress() + "/sprofile"
