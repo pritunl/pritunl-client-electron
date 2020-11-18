a9b10089c3937ac67ac3c9a6b97a1775d09e0d06
Zachary Huff <zach.huff.386@gmail.com>
2020-11-18 12:17:32 -0500

    Refactor macos components

--- openvpn_osx_arm64/build_lz4.sh
+++
@@ -1,6 +0,0 @@
-export CFLAGS="-mmacosx-version-min=11.0"
-export CXXFLAGS="-mmacosx-version-min=11.0"
-export CPPFLAGS="-mmacosx-version-min=11.0"
-export LINKFLAGS="-mmacosx-version-min=11.0"
-
-make install PREFIX=`pwd`/../lz4
