fd5502c7abe95c8610912af5db2f77a0e3d6699e
Zachary Huff <zach.huff.386@gmail.com>
2020-11-18 01:01:45 -0500

    Add openvpn osx arm64 build

---
+++ openvpn_osx_arm64/build_iperf3.sh
@@ -0,0 +1,10 @@
+export CFLAGS="-mmacosx-version-min=11.0"
+export CXXFLAGS="-mmacosx-version-min=11.0"
+export CPPFLAGS="-mmacosx-version-min=11.0"
+export LINKFLAGS="-mmacosx-version-min=11.0"
+
+./bootstrap.sh
+./configure \
+  --disable-profiling \
+  --prefix=`pwd`/../iperf3a
+make install
