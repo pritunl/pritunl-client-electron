15e7a00e1229872a452fa2923614c6d9826b5a73
Zachary Huff <zach.huff.386@gmail.com>
2020-11-18 15:10:41 -0500

    Create new release

--- client/www/js/constants.js
+++ client/www/js/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   key: '',
-  version: '1.2.2615.73',
+  version: '1.2.2624.84',
   serviceHost: '127.0.0.1:9770',
   wg: false,
   unixSocket: false,
--- service/constants/constants.go
+++ service/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Version = "1.2.2615.73"
+	Version = "1.2.2624.84"
 )
 
 var (
