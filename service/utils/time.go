dac511114bac3cb2ba02365f5b2df4ee7a2ddb4b
Zachary Huff <zach.huff.386@gmail.com>
2021-08-29 15:40:28 -0400

    Move since abs in utils

--- service/utils/time.go
+++ service/utils/time.go
@@ -7,3 +7,11 @@ import (
 func SinceSafe(t time.Time) time.Duration {
 	return t.Sub(time.Now())
 }
+
+func SinceAbs(t time.Time) (s time.Duration) {
+	s = SinceSafe(t)
+	if s < 0 {
+		s = s * -1
+	}
+	return
+}
--- service/utils/utils.go
+++ service/utils/utils.go
@@ -941,11 +941,3 @@ func PidInit() (err error) {
 
 	return
 }
-
-func SinceAbs(t time.Time) (s time.Duration) {
-	s = utils.SinceSafe(t)
-	if s < 0 {
-		s = s * -1
-	}
-	return
-}
