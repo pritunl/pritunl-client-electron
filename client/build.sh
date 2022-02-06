f02631912fe7d9d9b56408b11af1907149b3336f
Zachary Huff <zach.huff.386@gmail.com>
2022-02-05 19:50:58 -0500

    Add client www build

---
+++ client/build.sh
@@ -0,0 +1,49 @@
+tsc
+
+# development
+rm -rf dist-dev/static
+mkdir -p dist-dev/static
+cp styles/global.css dist-dev/static/
+cp node_modules/normalize.css/normalize.css dist-dev/static/
+cp node_modules/@blueprintjs/core/lib/css/blueprint.css dist-dev/static/
+cp node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.eot dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.ttf dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist-dev/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist-dev/static/
+sed -i 's|../../resources/icons/||g' dist-dev/static/blueprint-icons.css
+
+npm link webpack
+webpack --config webpack.dev.config
+
+cp index.html dist-dev/index.html
+
+# production
+rm -rf dist/static
+mkdir -p dist/static
+cp styles/global.css dist/static/
+cp node_modules/normalize.css/normalize.css dist/static/
+cp node_modules/@blueprintjs/core/lib/css/blueprint.css dist/static/
+cp node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.eot dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.ttf dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist/static/
+cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist/static/
+sed -i 's|../../resources/icons/||g' dist/static/blueprint-icons.css
+
+webpack
+
+cp index_dist.html dist/index.html
+
+APP_HASH=`md5sum dist/static/app.js | cut -c1-6`
+
+mv dist/static/app.js dist/static/app.${APP_HASH}.js
+#mv dist/static/app.js.map dist/static/app.${APP_HASH}.js.map
+
+sed -i -e "s|static/app.js|static/app.${APP_HASH}.js|g" dist/index.html
