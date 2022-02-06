9698e5c4a6af6e971bd9f646578c0368017bfd1a
Zachary Huff <zach.huff.386@gmail.com>
2022-02-06 18:18:17 -0500

    Use config for client theme

--- client/app/Theme.ts
+++ client/app/Theme.ts
@@ -1,49 +1,22 @@
 /// <reference path="./References.d.ts"/>
-import * as SuperAgent from 'superagent';
-import * as Alert from './Alert';
-import * as Csrf from './Csrf';
-
-export let theme = 'dark';
+import * as Config from './Config';
 
 export function save(): Promise<void> {
-	return new Promise<void>((resolve, reject): void => {
-		SuperAgent
-			.put('/theme')
-			.send({
-				theme: theme,
-			})
-			.set('Accept', 'application/json')
-			.set('Csrf-Token', Csrf.token)
-			.end((err: any, res: SuperAgent.Response): void => {
-				if (res && res.status === 401) {
-					window.location.href = '/login';
-					resolve();
-					return;
-				}
-
-				if (err) {
-					Alert.errorRes(res, 'Failed to save theme');
-					reject(err);
-					return;
-				}
-
-				resolve();
-			});
-	});
+	return Config.save();
 }
 
 export function light(): void {
-	theme = 'light';
+	Config.setTheme('light');
 	document.body.className = '';
 }
 
 export function dark(): void {
-	theme = 'dark';
+	Config.setTheme('dark');
 	document.body.className = 'bp3-dark';
 }
 
 export function toggle(): void {
-	if (theme === 'light') {
+	if (Config.theme === 'light') {
 		dark()
 	} else {
 		light();
