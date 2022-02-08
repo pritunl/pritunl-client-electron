7682a795a61c4f2485353d0978d2a8c85a7ac436
Zachary Huff <zach.huff.386@gmail.com>
2022-02-07 21:03:15 -0500

    Update theme in client

--- client/app/Theme.ts
+++ client/app/Theme.ts
@@ -1,22 +1,22 @@
 /// <reference path="./References.d.ts"/>
-import * as Config from './Config';
+import Config from "./Config";
 
 export function save(): Promise<void> {
 	return Config.save();
 }
 
 export function light(): void {
-	Config.setTheme('light');
-	document.body.className = '';
+	Config.theme = "light";
+	document.body.className = "";
 }
 
 export function dark(): void {
-	Config.setTheme('dark');
-	document.body.className = 'bp3-dark';
+	Config.theme = "dark";
+	document.body.className = "bp3-dark";
 }
 
 export function toggle(): void {
-	if (Config.theme === 'light') {
+	if (Config.theme === "light") {
 		dark()
 	} else {
 		light();
