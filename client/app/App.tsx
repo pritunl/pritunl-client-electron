ec823064c5602de714c153374c3869b18b45e929
Zachary Huff <zach.huff.386@gmail.com>
2022-02-09 05:23:24 -0500

    Update client app init

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -1,19 +1,20 @@
 /// <reference path="References.d.ts"/>
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as Blueprint from '@blueprintjs/core';
-import Main from './components/Main';
-import * as Alert from './Alert';
-import * as Event from './Event';
-import * as Auth from './Auth';
-import * as Config from './Config';
-import * as Theme from './Theme';
-import * as Constants from './Constants';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as Blueprint from "@blueprintjs/core";
+import Main from "./components/Main";
+import * as Alert from "./Alert";
+import * as Event from "./Event";
+import * as Auth from "./Auth";
+import * as Theme from "./Theme";
+import * as Constants from "./Constants";
+import Config from "./Config";
 
 Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
+Alert.init();
 
 Config.load().then((): void => {
-	if (Config.theme === 'light') {
+	if (Config.theme === "light") {
 		Theme.light();
 	} else {
 		Theme.dark();
@@ -21,11 +22,10 @@ Config.load().then((): void => {
 
 	Constants.load();
 	Auth.load();
-	Alert.init();
 	Event.init();
 
 	ReactDOM.render(
 		<div><Main/></div>,
-		document.getElementById('app'),
+		document.getElementById("app"),
 	);
 });
