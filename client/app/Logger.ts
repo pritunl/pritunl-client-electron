1a1e0f1e0fd8d871c7179f2c15648b70cee953f3
Zachary Huff <zach.huff.386@gmail.com>
2022-02-10 06:18:42 -0500

    Update errors in app logger

--- client/app/Logger.ts
+++ client/app/Logger.ts
@@ -1,19 +1,21 @@
 /// <reference path="./References.d.ts"/>
 import * as Constants from "./Constants";
 import * as Alert from "./Alert";
+import * as Errors from "./Errors";
+import * as Paths from "./Paths";
 import fs from "fs";
-import path from "path";
-
-function logPath(): string {
-	return path.join(Constants.dataPath, "pritunl.json");
-}
 
 function push(level: string, msg: string): void {
 	let time = new Date();
 	msg = "[" + time.getFullYear() + "-" + (time.getMonth() + 1) + "-" +
 		time.getDate() + " " + time.getHours() + ":" + time.getMinutes() + ":" +
 		time.getSeconds() + "][" + level  + "] " + msg + "\n";
-	fs.appendFile(logPath(), msg, (err: Error): void => {})
+	fs.appendFile(Paths.log(), msg, (err: Error): void => {
+		if (err) {
+			err = new Errors.WriteError(err, "Logger: Failed to write log");
+			Alert.error(err.message, 10)
+		}
+	})
 }
 
 export function info(msg: string): void {
