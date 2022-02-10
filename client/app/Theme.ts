4fbc562ce449beecc044c5e100b25f8a7b8ea9d9
Zachary Huff <zach.huff.386@gmail.com>
2022-02-10 06:19:13 -0500

    Add events to app theme

--- client/app/Theme.ts
+++ client/app/Theme.ts
@@ -1,5 +1,12 @@
 /// <reference path="./References.d.ts"/>
 import Config from "./Config";
+import * as GlobalTypes from "./types/GlobalTypes";
+
+export interface Callback {
+	(): void;
+}
+
+let callbacks: Set<Callback> = new Set<Callback>();
 
 export function save(): Promise<void> {
 	return Config.save();
@@ -8,11 +15,17 @@ export function save(): Promise<void> {
 export function light(): void {
 	Config.theme = "light";
 	document.body.className = "";
+	callbacks.forEach((callback: Callback): void => {
+		callback();
+	})
 }
 
 export function dark(): void {
 	Config.theme = "dark";
 	document.body.className = "bp3-dark";
+	callbacks.forEach((callback: Callback): void => {
+		callback();
+	})
 }
 
 export function toggle(): void {
@@ -21,4 +34,24 @@ export function toggle(): void {
 	} else {
 		light();
 	}
+}
+
+export function theme(): string {
+	return Config.theme;
+}
+
+export function editorTheme(): string {
+	if (Config.theme === "light") {
+		return "eclipse";
+	} else {
+		return "dracula";
+	}
+}
+
+export function addChangeListener(callback: Callback): void {
+	callbacks.add(callback);
+}
+
+export function removeChangeListener(callback: () => void): void {
+	callbacks.delete(callback);
 }
