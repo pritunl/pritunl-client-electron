6a0a3b1cfaf6b3c3a2103e744da1f4f5577f1c1b
Zachary Huff <zach.huff.386@gmail.com>
2022-02-13 11:53:48 -0500

    Add paths to profile types

--- client/app/types/ProfileTypes.ts
+++ client/app/types/ProfileTypes.ts
@@ -1,36 +1,45 @@
 /// <reference path="../References.d.ts"/>
-export const SYNC = 'profile.sync';
-export const TRAVERSE = 'profile.traverse';
-export const FILTER = 'profile.filter';
-export const CHANGE = 'profile.change';
+import path from "path"
+import * as Constants from "../Constants"
+
+export const SYNC = "profile.sync"
+export const TRAVERSE = "profile.traverse"
+export const FILTER = "profile.filter"
+export const CHANGE = "profile.change"
 
 export interface Profile {
-	id?: string;
-	system?: boolean;
-	name?: string;
-	uv_name?: string;
-	state?: string;
-	wg?: boolean;
-	disable_reconnect?: boolean;
-	last_mode?: string;
-	organization_id?: string;
-	organization?: string;
-	server_id?: string;
-	server?: string;
-	user_id?: string;
-	user?: string;
-	pre_connect_msg?: string;
-	password_mode?: string;
-	token?: boolean;
-	token_ttl?: number;
-	sync_hosts?: string[];
-	sync_hash?: string;
-	sync_secret?: string;
-	sync_token?: string;
-	server_public_key?: string[];
-	server_box_public_key?: string;
-	status?: string;
-	timestamp?: number;
+	id?: string
+	system?: boolean
+	name?: string
+	uv_name?: string
+	state?: string
+	wg?: boolean
+	disable_reconnect?: boolean
+	last_mode?: string
+	organization_id?: string
+	organization?: string
+	server_id?: string
+	server?: string
+	user_id?: string
+	user?: string
+	pre_connect_msg?: string
+	password_mode?: string
+	token?: boolean
+	token_ttl?: number
+	sync_hosts?: string[]
+	sync_hash?: string
+	sync_secret?: string
+	sync_token?: string
+	server_public_key?: string[]
+	server_box_public_key?: string
+	status?: string
+	timestamp?: number
+
+	formattedName(): string
+	formattedUptime(): string
+	formatedHosts(): string[]
+	confPath(): string
+	dataPath(): string
 }
 
 export interface Filter {
@@ -142,6 +151,14 @@ export function New(data: Profile): Profile {
 		}
 
 		return hosts;
+	}
+
+	data.confPath = function(): string {
+		return path.join(Constants.dataPath, "profiles", this.id + ".conf")
+	}
+
+	data.dataPath = function(): string {
+		return path.join(Constants.dataPath, "profiles", this.id + ".ovpn")
 	}
 
 	return data;
