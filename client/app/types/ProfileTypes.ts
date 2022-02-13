44a5218959a17077720b0c53a970b3e0832da9d7
Zachary Huff <zach.huff.386@gmail.com>
2022-02-13 09:55:25 -0500

    Add new to profile types

--- client/app/types/ProfileTypes.ts
+++ client/app/types/ProfileTypes.ts
@@ -74,3 +74,75 @@ export interface ProfileData {
 	timeout?: number;
 	data?: string;
 }
+
+export function New(data: Profile): Profile {
+	data.formattedName = function(): string {
+		return this.server + " (" + this.user + ")"
+	}
+
+	data.formattedUptime = function(): string {
+		if (!this.timestamp || this.status !== 'connected') {
+			return '';
+		}
+
+		let  curTime = Math.floor((new Date).getTime() / 1000);
+
+		let uptime = curTime - this.timestamp;
+		let units: number;
+		let unitStr: string;
+		let uptimeItems: string[] =[];
+
+		if (uptime > 86400) {
+			units = Math.floor(uptime / 86400);
+			uptime -= units * 86400;
+			unitStr = units + ' day';
+			if (units > 1) {
+				unitStr += 's';
+			}
+			uptimeItems.push(unitStr);
+		}
+
+		if (uptime > 3600) {
+			units = Math.floor(uptime / 3600);
+			uptime -= units * 3600;
+			unitStr = units + ' hour';
+			if (units > 1) {
+				unitStr += 's';
+			}
+			uptimeItems.push(unitStr);
+		}
+
+		if (uptime > 60) {
+			units = Math.floor(uptime / 60);
+			uptime -= units * 60;
+			unitStr = units + ' min';
+			if (units > 1) {
+				unitStr += 's';
+			}
+			uptimeItems.push(unitStr);
+		}
+
+		if (uptime) {
+			unitStr = uptime + ' sec';
+			if (uptime > 1) {
+				unitStr += 's';
+			}
+			uptimeItems.push(unitStr);
+		}
+
+		return uptimeItems.join(' ');
+	}
+
+	data.formatedHosts = function(): string[] {
+		let hosts: string[] = [];
+
+		for (let hostAddr of this.sync_hosts) {
+			let url = new URL(hostAddr);
+			hosts.push(url.hostname + (url.port ? (':' + url.port) : ''));
+		}
+
+		return hosts;
+	}
+
+	return data;
+}
