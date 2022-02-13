03fb8e4f07087c6a39a9e72f6240f05f12ae1738
Zachary Huff <zach.huff.386@gmail.com>
2022-02-13 11:52:38 -0500

    Update service actions

--- client/app/actions/ServiceActions.ts
+++ client/app/actions/ServiceActions.ts
@@ -1,52 +1,34 @@
 /// <reference path="../References.d.ts"/>
-import * as SuperAgent from 'superagent';
-import * as Auth from "../Auth";
-import * as Constants from '../Constants';
-import * as ProfileTypes from '../types/ProfileTypes';
-import Dispatcher from '../dispatcher/Dispatcher';
-import EventDispatcher from '../dispatcher/EventDispatcher';
-import * as Alert from '../Alert';
-import * as Paths from '../Paths';
-import Loader from '../Loader';
-import * as MiscUtils from '../utils/MiscUtils';
+import * as SuperAgent from "superagent"
+import * as ProfileTypes from "../types/ProfileTypes"
+import * as Alert from "../Alert"
+import * as RequestUtils from "../utils/RequestUtils"
+import Loader from "../Loader"
 
 export function connect(prfl: ProfileTypes.ProfileData,
 		noLoading?: boolean): Promise<void> {
-	let loader: Loader;
+	let loader: Loader
 	if (!noLoading) {
-		loader = new Loader().loading();
+		loader = new Loader().loading()
 	}
 
 	return new Promise<void>((resolve, reject): void => {
-		let req = SuperAgent
-			.post('/profile')
+		RequestUtils
+			.post("/profile")
 			.send(prfl)
-			.set('Accept', 'application/json')
-			.set('User-Agent', 'pritunl')
-			.set('Auth-Token', Auth.token);
-
-		if (Constants.unix) {
-			req.set('Host', 'unix');
-		}
-
-		req.end((err: any, res: SuperAgent.Response): void => {
+			.set("Accept", "application/json")
+			.end((err: any, res: SuperAgent.Response): void => {
 				if (loader) {
-					loader.done();
-				}
-
-				if (res && res.status === 401) {
-					window.location.href = '/login';
-					resolve();
-					return;
+					loader.done()
 				}
 
 				if (err) {
-					Alert.errorRes(res, 'Profile connect failed');
-					reject(err);
-					return;
+					Alert.errorRes(res, "Profile connect failed")
+					reject(err)
+					return
 				}
 
-				resolve();
-			});
-	});
+				resolve()
+			})
+	})
 }
