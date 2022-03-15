07c49edefcfa33f511a48a5e27e12cb8cdf44638
Zachary Huff <zach.huff.386@gmail.com>
2022-03-15 07:25:41 -0400

    Add import tar command

--- cli/cmd/add.go
+++ cli/cmd/add.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-client-electron/cli/errortypes"
 	"github.com/pritunl/pritunl-client-electron/cli/sprofile"
@@ -8,20 +10,34 @@ import (
 )
 
 var AddCmd = &cobra.Command{
-	Use:   "add [profile_uri]",
+	Use:   "add [profile_uri|tar_path]",
 	Short: "Add profile",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			err := errortypes.NotFoundError{
-				errors.New("cmd: Missing profile URI"),
+				errors.New("cmd: Missing profile URI or path"),
 			}
 			panic(err)
 		}
 
-		err := sprofile.ImportUri(args[0])
-		if err != nil {
-			panic(err)
-			return
+		path := args[0]
+		if strings.HasPrefix(path, "http://") ||
+			strings.HasPrefix(path, "https://") ||
+			strings.HasPrefix(path, "pritunl://") ||
+			strings.HasPrefix(path, "pritunls://") {
+
+			err := sprofile.ImportUri(path)
+			if err != nil {
+				panic(err)
+				return
+			}
+		} else {
+			err := sprofile.ImportTar(path)
+			if err != nil {
+				panic(err)
+				return
+			}
 		}
+
 	},
 }
