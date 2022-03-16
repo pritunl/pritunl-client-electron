967449fc30b381eef3f110bf7cdcd08cb65eda5b
Zachary Huff <zach.huff.386@gmail.com>
2022-03-16 11:09:59 -0400

    Update main logo

--- client/main/Main.tsx
+++ client/main/Main.tsx
@@ -1,34 +1,34 @@
-import process from "process";
-import path from "path";
-import electron from "electron";
+import process from "process"
+import path from "path"
+import electron from "electron"
 
 let connTray
 let disconnTray
 if (process.platform === "darwin") {
-	connTray = path.join(__dirname, "img",
+	connTray = path.join(__dirname, "..", "img",
 		"tray_connected_osxTemplate.png")
-	disconnTray = path.join(__dirname, "img",
+	disconnTray = path.join(__dirname, "..", "img",
 		"tray_disconnected_osxTemplate.png")
 } else if (process.platform === "win32") {
-	connTray = path.join(__dirname, "img",
+	connTray = path.join(__dirname, "..", "img",
 		"tray_connected_win.png")
-	disconnTray = path.join(__dirname, "img",
-		"tray_disconnected_win.png");
+	disconnTray = path.join(__dirname, "..", "img",
+		"tray_disconnected_win.png")
 } else if (process.platform === "linux") {
-	connTray = path.join(__dirname, "img",
+	connTray = path.join(__dirname, "..", "img",
 		"tray_connected_linux_light.png")
-	disconnTray = path.join(__dirname, "img",
+	disconnTray = path.join(__dirname, "..", "img",
 		"tray_disconnected_linux_light.png")
 } else {
-	connTray = path.join(__dirname, "img",
+	connTray = path.join(__dirname, "..", "img",
 		"tray_connected.png")
-	disconnTray = path.join(__dirname, "img",
+	disconnTray = path.join(__dirname, "..", "img",
 		"tray_disconnected.png")
 }
-let icon = path.join(__dirname, "img", "logo.png")
+let icon = path.join(__dirname, "..", "img", "logo.png")
 
 let orig = true
-if (process.argv.indexOf('--beta') !== -1) {
+if (process.argv.indexOf("--beta") !== -1) {
 	orig = false
 }
 
@@ -37,40 +37,40 @@ if (orig) {
 }
 
 class Main {
-	window: electron.BrowserWindow;
+	window: electron.BrowserWindow
 
 	mainWindow(): void {
-		let width: number;
-		let height: number;
-		let minWidth: number;
-		let minHeight: number;
-		let maxWidth: number;
-		let maxHeight: number;
-		if (process.platform === 'darwin') {
-			width = 340;
-			height = 423;
-			minWidth = 304;
-			minHeight = 352;
-			maxWidth = 540;
-			maxHeight = 642;
+		let width: number
+		let height: number
+		let minWidth: number
+		let minHeight: number
+		let maxWidth: number
+		let maxHeight: number
+		if (process.platform === "darwin") {
+			width = 340
+			height = 423
+			minWidth = 304
+			minHeight = 352
+			maxWidth = 540
+			maxHeight = 642
 		} else {
-			width = 420;
-			height = 528;
-			minWidth = 380;
-			minHeight = 440;
-			maxWidth = 670;
-			maxHeight = 800;
+			width = 420
+			height = 528
+			minWidth = 380
+			minHeight = 440
+			maxWidth = 670
+			maxHeight = 800
 		}
 
-		let zoomFactor = 1;
-		if (process.platform === 'darwin') {
-			zoomFactor = 0.8;
+		let zoomFactor = 1
+		if (process.platform === "darwin") {
+			zoomFactor = 0.8
 		}
 
 		if (orig) {
 			this.window = new electron.BrowserWindow({
-				title: 'Pritunl Client',
-				icon: path.join(__dirname, '..', 'logo.png'),
+				title: "Pritunl Client",
+				icon: path.join(__dirname, "..", "logo.png"),
 				frame: true,
 				autoHideMenuBar: true,
 				fullscreen: false,
@@ -81,7 +81,7 @@ class Main {
 				minHeight: minHeight,
 				maxWidth: maxWidth,
 				maxHeight: maxHeight,
-				backgroundColor: '#151719',
+				backgroundColor: "#151719",
 				webPreferences: {
 					zoomFactor: zoomFactor,
 					devTools: true,
@@ -89,11 +89,11 @@ class Main {
 					nodeIntegration: true,
 					contextIsolation: false
 				} as any
-			});
+			})
 		} else {
 			this.window = new electron.BrowserWindow({
-				title: 'Pritunl Client',
-				icon: path.join(__dirname, '..', 'logo.png'),
+				title: "Pritunl Client",
+				icon: path.join(__dirname, "..", "logo.png"),
 				frame: true,
 				autoHideMenuBar: true,
 				fullscreen: false,
@@ -104,154 +104,241 @@ class Main {
 				minHeight: minHeight,
 				maxWidth: maxWidth,
 				maxHeight: maxHeight,
-				backgroundColor: '#151719',
+				backgroundColor: "#151719",
 				webPreferences: {
 					zoomFactor: zoomFactor,
 					devTools: true,
 					nodeIntegration: true,
 					contextIsolation: false
 				}
-			});
+			})
 		}
 
-		this.window.on('closed', (): void => {
-			electron.app.quit();
-			this.window = null;
-		});
+		this.window.on("closed", (): void => {
+			electron.app.quit()
+			this.window = null
+		})
 
-		let shown = false;
-		this.window.on('ready-to-show', (): void => {
+		let shown = false
+		this.window.on("ready-to-show", (): void => {
 			if (shown) {
-				return;
+				return
 			}
-			shown = true;
-			this.window.show();
+			shown = true
+			this.window.show()
 
-			if (process.argv.indexOf('--dev-tools') !== -1) {
-				this.window.webContents.openDevTools();
+			if (process.argv.indexOf("--dev-tools") !== -1) {
+				this.window.webContents.openDevTools()
 			}
-		});
+		})
 		setTimeout((): void => {
 			if (shown) {
-				return;
+				return
 			}
-			shown = true;
-			this.window.show();
+			shown = true
+			this.window.show()
 
-			if (process.argv.indexOf('--dev-tools') !== -1) {
-				this.window.webContents.openDevTools();
+			if (process.argv.indexOf("--dev-tools") !== -1) {
+				this.window.webContents.openDevTools()
 			}
-		}, 800);
+		}, 800)
 
-		let indexUrl = ''
+		let indexUrl = ""
 		if (orig) {
-			indexUrl = 'file://' + path.join(__dirname, '..',
-				'index_orig.html');
+			indexUrl = "file://" + path.join(__dirname, "..",
+				"index_orig.html")
 		} else {
-			indexUrl = 'file://' + path.join(__dirname, '..',
-				'index.html');
+			indexUrl = "file://" + path.join(__dirname, "..",
+				"index.html")
 		}
-		indexUrl += '?dev=' + (process.argv.indexOf('--dev') !== -1 ?
-			'true' : 'false');
-		indexUrl += '&dataPath=' + encodeURIComponent(
-			electron.app.getPath('userData'));
+		indexUrl += "?dev=" + (process.argv.indexOf("--dev") !== -1 ?
+			"true" : "false")
+		indexUrl += "&dataPath=" + encodeURIComponent(
+			electron.app.getPath("userData"))
 
-		this.window.loadURL(indexUrl);
+		this.window.loadURL(indexUrl, {
+			userAgent: "pritunl",
+		})
 
 		if (electron.app.dock) {
-			electron.app.dock.show();
+			electron.app.dock.show()
 		}
 	}
 
 	run(): void {
-		this.mainWindow();
+		this.mainWindow()
 	}
 }
 
-process.on('uncaughtException', function (error) {
-	let errorMsg: string;
+process.on("uncaughtException", function (error) {
+	let errorMsg: string
 	if (error && error.stack) {
-		errorMsg = error.stack;
+		errorMsg = error.stack
 	} else {
-		errorMsg = String(error);
+		errorMsg = String(error)
 	}
 
 	electron.dialog.showMessageBox(null, {
-		type: 'error',
-		buttons: ['Exit'],
-		title: 'Pritunl Client - Process Error',
-		message: 'Error occured in main process:\n\n' + errorMsg,
+		type: "error",
+		buttons: ["Exit"],
+		title: "Pritunl Client - Process Error",
+		message: "Error occured in main process:\n\n" + errorMsg,
 	}).then(function() {
-		electron.app.quit();
-	});
-});
+		electron.app.quit()
+	})
+})
 
 if (electron.app.dock) {
-	electron.app.dock.hide();
+	electron.app.dock.hide()
 }
 
-electron.app.on('window-all-closed', (): void => {
-	// config.reload((): void => {
-	// 	if (true) {
-	// 		electron.app.quit();
-	// 	} else {
-	// 		if (electron.app.dock) {
-	// 			electron.app.dock.hide();
+electron.app.on("window-all-closed", (): void => {
+	// try {
+	// 	config.reload((): void => {
+	// 		if (true) {
+	// 			electron.app.quit()
+	// 		} else {
+	// 			if (electron.app.dock) {
+	// 				electron.app.dock.hide()
+	// 			}
 	// 		}
-	// 	}
-	// });
-});
+	// 	})
+	// } catch (error) {
+	// 	throw error
+	// }
+})
 
-electron.app.on('open-file', (): void => {
-	let main = new Main();
-	main.run();
-});
+electron.app.on("open-file", (): void => {
+	try {
+		let main = new Main()
+		main.run()
+	} catch (error) {
+		throw error
+	}
+})
 
-electron.app.on('open-url', (): void => {
-	let main = new Main();
-	main.run();
-});
+electron.app.on("open-url", (): void => {
+	try {
+		let main = new Main()
+		main.run()
+	} catch (error) {
+		throw error
+	}
+})
 
-electron.app.on('activate', (): void => {
-	let main = new Main();
-	main.run();
-});
+electron.app.on("activate", (): void => {
+	try {
+		let main = new Main()
+		main.run()
+	} catch (error) {
+		throw error
+	}
+})
 
-electron.app.on('quit', (): void => {
-	electron.app.quit();
-});
+electron.app.on("quit", (): void => {
+	try {
+		electron.app.quit()
+	} catch (error) {
+		throw error
+	}
+})
 
-electron.app.on('ready', (): void => {
-	let tray = new electron.Tray(path.join(__dirname, '..', 'logo.png'));
+electron.app.on("ready", (): void => {
+	try {
+		let tray = new electron.Tray(disconnTray)
 
-	tray.on('click', function() {
-		let main = new Main();
-		main.run();
-	});
-	tray.on('double-click', function() {
-		let main = new Main();
-		main.run();
-	});
+		tray.on("click", function() {
+			let main = new Main()
+			main.run()
+		})
+		tray.on("double-click", function() {
+			let main = new Main()
+			main.run()
+		})
 
-	let trayMenu = electron.Menu.buildFromTemplate([
-		{
-			label: 'Pritunl vTODO',
-			click: function () {
-				let main = new Main();
-				main.run();
+		let trayMenu = electron.Menu.buildFromTemplate([
+			{
+				label: "Pritunl vTODO",
+				click: function () {
+					let main = new Main()
+					main.run()
+				}
+			},
+			{
+				label: "Exit",
+				click: function() {
+					electron.app.quit()
+				}
 			}
-		},
-		{
-			label: 'Exit',
-			click: function() {
-				electron.app.quit();
+		])
+
+		tray.setToolTip("Pritunl vTODO")
+		tray.setContextMenu(trayMenu)
+
+		let appMenu = electron.Menu.buildFromTemplate([
+			{
+				label: "Pritunl",
+				submenu: [
+					{
+						label: "Pritunl vTODO",
+					},
+					{
+						label: "Close",
+						accelerator: "CmdOrCtrl+Q",
+						role: "close",
+					},
+					{
+						label: "Exit",
+						click: function() {
+							electron.app.quit()
+						},
+					},
+				],
+			},
+			{
+				label: "Edit",
+				submenu: [
+					{
+						label: "Undo",
+						accelerator: "CmdOrCtrl+Z",
+						role: "undo",
+					},
+					{
+						label: "Redo",
+						accelerator: "Shift+CmdOrCtrl+Z",
+						role: "redo",
+					},
+					{
+						type: "separator",
+					},
+					{
+						label: "Cut",
+						accelerator: "CmdOrCtrl+X",
+						role: "cut",
+					},
+					{
+						label: "Copy",
+						accelerator: "CmdOrCtrl+C",
+						role: "copy",
+					},
+					{
+						label: "Paste",
+						accelerator: "CmdOrCtrl+V",
+						role: "paste",
+					},
+					{
+						label: "Select All",
+						accelerator: "CmdOrCtrl+A",
+						role: "selectall",
+					},
+				],
 			}
-		}
-	]);
+		] as any)
+		electron.Menu.setApplicationMenu(appMenu)
 
-	tray.setToolTip('Pritunl vTODO');
-	tray.setContextMenu(trayMenu);
-
-	let main = new Main();
-	main.run();
-});
+		let main = new Main()
+		main.run()
+	} catch (error) {
+		throw error
+	}
+})
