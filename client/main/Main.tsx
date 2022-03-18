eaca7784559bcac9a4521b24e4eafefe03fd3c6c
Zachary Huff <zach.huff.386@gmail.com>
2022-03-18 12:35:03 -0400

    Update main init

--- client/main/Main.tsx
+++ client/main/Main.tsx
@@ -1,33 +1,12 @@
 import process from "process"
 import path from "path"
 import electron from "electron"
-
 import * as Service from "./Service"
+import Config from "./Config"
 
-let connTray = ""
-let disconnTray = ""
-if (process.platform === "darwin") {
-	connTray = path.join(__dirname, "..", "img",
-		"tray_connected_osxTemplate.png")
-	disconnTray = path.join(__dirname, "..", "img",
-		"tray_disconnected_osxTemplate.png")
-} else if (process.platform === "win32") {
-	connTray = path.join(__dirname, "..", "img",
-		"tray_connected_win.png")
-	disconnTray = path.join(__dirname, "..", "img",
-		"tray_disconnected_win.png")
-} else if (process.platform === "linux") {
-	connTray = path.join(__dirname, "..", "img",
-		"tray_connected_linux_light.png")
-	disconnTray = path.join(__dirname, "..", "img",
-		"tray_disconnected_linux_light.png")
-} else {
-	connTray = path.join(__dirname, "..", "img",
-		"tray_connected.png")
-	disconnTray = path.join(__dirname, "..", "img",
-		"tray_disconnected.png")
-}
-let icon = path.join(__dirname, "..", "img", "logo.png")
+let tray: electron.Tray
+let awaken: boolean
+let ready: boolean
 
 let orig = true
 if (process.argv.indexOf("--beta") !== -1) {
@@ -37,6 +16,35 @@ if (process.argv.indexOf("--beta") !== -1) {
 if (orig) {
 	require("@electron/remote/main").initialize()
 }
+
+if (electron.app.dock) {
+	electron.app.dock.hide()
+}
+
+process.on("uncaughtException", function (error) {
+	let errorMsg: string
+	if (error && error.stack) {
+		errorMsg = error.stack
+	} else {
+		errorMsg = String(error)
+	}
+
+	electron.dialog.showMessageBox(null, {
+		type: "error",
+		buttons: ["Exit"],
+		title: "Pritunl Client - Process Error",
+		message: "Error occured in main process:\n\n" + errorMsg,
+	}).then(function() {
+		electron.app.quit()
+	})
+})
+
+Service.wakeup().then((awake: boolean) => {
+	awaken = awake
+	if (ready) {
+		init()
+	}
+})
 
 class Main {
 	window: electron.BrowserWindow
@@ -117,7 +125,9 @@ class Main {
 		}
 
 		this.window.on("closed", (): void => {
-			electron.app.quit()
+			if (Config.disable_tray_icon || !tray) {
+				electron.app.quit()
+			}
 			this.window = null
 		})
 
@@ -172,42 +182,182 @@ class Main {
 	}
 }
 
-process.on("uncaughtException", function (error) {
-	let errorMsg: string
-	if (error && error.stack) {
-		errorMsg = error.stack
-	} else {
-		errorMsg = String(error)
+function initTray() {
+	tray = new electron.Tray(getTrayIcon())
+
+	tray.on("click", function() {
+		let main = new Main()
+		main.run()
+	})
+	tray.on("double-click", function() {
+		let main = new Main()
+		main.run()
+	})
+
+	let trayMenu = electron.Menu.buildFromTemplate([
+		{
+			label: "Pritunl vTODO",
+			click: function () {
+				let main = new Main()
+				main.run()
+			}
+		},
+		{
+			label: "Exit",
+			click: function() {
+				electron.app.quit()
+			}
+		}
+	])
+
+	tray.setToolTip("Pritunl vTODO")
+	tray.setContextMenu(trayMenu)
+}
+
+function initAppMenu() {
+	let appMenu = electron.Menu.buildFromTemplate([
+		{
+			label: "Pritunl",
+			submenu: [
+				{
+					label: "Pritunl vTODO",
+				},
+				{
+					label: "Close",
+					accelerator: "CmdOrCtrl+Q",
+					role: "close",
+				},
+				{
+					label: "Exit",
+					click: function() {
+						electron.app.quit()
+					},
+				},
+			],
+		},
+		{
+			label: "Edit",
+			submenu: [
+				{
+					label: "Undo",
+					accelerator: "CmdOrCtrl+Z",
+					role: "undo",
+				},
+				{
+					label: "Redo",
+					accelerator: "Shift+CmdOrCtrl+Z",
+					role: "redo",
+				},
+				{
+					type: "separator",
+				},
+				{
+					label: "Cut",
+					accelerator: "CmdOrCtrl+X",
+					role: "cut",
+				},
+				{
+					label: "Copy",
+					accelerator: "CmdOrCtrl+C",
+					role: "copy",
+				},
+				{
+					label: "Paste",
+					accelerator: "CmdOrCtrl+V",
+					role: "paste",
+				},
+				{
+					label: "Select All",
+					accelerator: "CmdOrCtrl+A",
+					role: "selectall",
+				},
+			],
+		}
+	] as any)
+	electron.Menu.setApplicationMenu(appMenu)
+}
+
+function init() {
+	if (awaken === undefined) {
+		return
+	} else if (awaken) {
+		electron.app.quit()
+		return
 	}
 
-	electron.dialog.showMessageBox(null, {
-		type: "error",
-		buttons: ["Exit"],
-		title: "Pritunl Client - Process Error",
-		message: "Error occured in main process:\n\n" + errorMsg,
-	}).then(function() {
-		electron.app.quit()
-	})
-})
+	Config.load().then(() => {
+		Service.connect(process.argv.indexOf("--dev") !== -1).then(() => {
+			if (process.argv.indexOf("--no-main") !== -1) {
+				if (Config.disable_tray_icon) {
+					electron.app.quit()
+					return
+				}
+			} else {
+				let main = new Main()
+				main.run()
+			}
 
-if (electron.app.dock) {
-	electron.app.dock.hide()
+			initAppMenu()
+
+			if (!Config.disable_tray_icon) {
+				initTray()
+			}
+
+			Service.subscribe((event: Service.Event): void => {
+				if (event.type === "wakeup") {
+					Service.send("awake")
+
+					let main = new Main()
+					main.run()
+				}
+			})
+		})
+	})
+}
+
+function getTrayIcon(): string {
+	let connTray = ""
+	let disconnTray = ""
+
+	if (process.platform === "darwin") {
+		connTray = path.join(__dirname, "..", "img",
+			"tray_connected_osxTemplate.png")
+		disconnTray = path.join(__dirname, "..", "img",
+			"tray_disconnected_osxTemplate.png")
+	} else if (process.platform === "win32") {
+		connTray = path.join(__dirname, "..", "img",
+			"tray_connected_win.png")
+		disconnTray = path.join(__dirname, "..", "img",
+			"tray_disconnected_win.png")
+	} else if (process.platform === "linux") {
+		connTray = path.join(__dirname, "..", "img",
+			"tray_connected_linux_light.png")
+		disconnTray = path.join(__dirname, "..", "img",
+			"tray_disconnected_linux_light.png")
+	} else {
+		connTray = path.join(__dirname, "..", "img",
+			"tray_connected.png")
+		disconnTray = path.join(__dirname, "..", "img",
+			"tray_disconnected.png")
+	}
+
+	return disconnTray
 }
 
 electron.app.on("window-all-closed", (): void => {
-	// try {
-	// 	config.reload((): void => {
-	// 		if (true) {
-	// 			electron.app.quit()
-	// 		} else {
-	// 			if (electron.app.dock) {
-	// 				electron.app.dock.hide()
-	// 			}
-	// 		}
-	// 	})
-	// } catch (error) {
-	// 	throw error
-	// }
+	try {
+		Config.load().then(() => {
+			if (Config.disable_tray_icon || !tray) {
+				electron.app.quit()
+			} else {
+				if (electron.app.dock) {
+					electron.app.dock.hide()
+				}
+			}
+		})
+	} catch (error) {
+		throw error
+	}
 })
 
 electron.app.on("open-file", (): void => {
@@ -247,116 +397,8 @@ electron.app.on("quit", (): void => {
 
 electron.app.on("ready", (): void => {
 	try {
-		let tray = new electron.Tray(disconnTray)
-
-		tray.on("click", function() {
-			let main = new Main()
-			main.run()
-		})
-		tray.on("double-click", function() {
-			let main = new Main()
-			main.run()
-		})
-
-		let trayMenu = electron.Menu.buildFromTemplate([
-			{
-				label: "Pritunl vTODO",
-				click: function () {
-					let main = new Main()
-					main.run()
-				}
-			},
-			{
-				label: "Exit",
-				click: function() {
-					electron.app.quit()
-				}
-			}
-		])
-
-		tray.setToolTip("Pritunl vTODO")
-		tray.setContextMenu(trayMenu)
-
-		let appMenu = electron.Menu.buildFromTemplate([
-			{
-				label: "Pritunl",
-				submenu: [
-					{
-						label: "Pritunl vTODO",
-					},
-					{
-						label: "Close",
-						accelerator: "CmdOrCtrl+Q",
-						role: "close",
-					},
-					{
-						label: "Exit",
-						click: function() {
-							electron.app.quit()
-						},
-					},
-				],
-			},
-			{
-				label: "Edit",
-				submenu: [
-					{
-						label: "Undo",
-						accelerator: "CmdOrCtrl+Z",
-						role: "undo",
-					},
-					{
-						label: "Redo",
-						accelerator: "Shift+CmdOrCtrl+Z",
-						role: "redo",
-					},
-					{
-						type: "separator",
-					},
-					{
-						label: "Cut",
-						accelerator: "CmdOrCtrl+X",
-						role: "cut",
-					},
-					{
-						label: "Copy",
-						accelerator: "CmdOrCtrl+C",
-						role: "copy",
-					},
-					{
-						label: "Paste",
-						accelerator: "CmdOrCtrl+V",
-						role: "paste",
-					},
-					{
-						label: "Select All",
-						accelerator: "CmdOrCtrl+A",
-						role: "selectall",
-					},
-				],
-			}
-		] as any)
-		electron.Menu.setApplicationMenu(appMenu)
-
-		Service.connect(process.argv.indexOf("--dev") !== -1).then(() => {
-			Service.wakeup().then((awake: boolean) => {
-				if (awake) {
-					electron.app.quit()
-				} else {
-					let main = new Main()
-					main.run()
-
-					Service.subscribe((event: Service.Event): void => {
-						if (event.type === "wakeup") {
-							Service.send("awake")
-
-							let main = new Main()
-							main.run()
-						}
-					})
-				}
-			})
-		})
+		ready = true
+		init()
 	} catch (error) {
 		throw error
 	}
