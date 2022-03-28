175914c391c73f75055bc670c384cf50d8a084e5
Zachary Huff <zach.huff.386@gmail.com>
2022-03-28 07:50:15 -0400

    Add version and upgrade to state handler

--- service/handlers/state.go
+++ service/handlers/state.go
@@ -4,16 +4,22 @@ import (
 	"runtime"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pritunl/pritunl-client-electron/service/constants"
 	"github.com/pritunl/pritunl-client-electron/service/profile"
+	"github.com/pritunl/pritunl-client-electron/service/update"
 )
 
 type stateData struct {
-	Wg bool `json:"wg"`
+	Wg      bool   `json:"wg"`
+	Version string `json:"version"`
+	Upgrade bool   `json:"upgrade"`
 }
 
 func stateGet(c *gin.Context) {
 	data := &stateData{
-		Wg: false,
+		Wg:      false,
+		Version: constants.Version,
+		Upgrade: update.Upgrade,
 	}
 
 	switch runtime.GOOS {
