45286fdbaa859bb14585c1f9c1e88ae047a88be2
Zachary Huff <zach.huff.386@gmail.com>
2022-03-29 21:17:15 -0400

    Update app auth

--- client/app/Auth.ts
+++ client/app/Auth.ts
@@ -1,6 +1,4 @@
 /// <reference path="./References.d.ts"/>
-import * as SuperAgent from 'superagent';
-import * as Theme from './Theme';
 import * as Constants from './Constants';
 import fs from "fs";
 
@@ -8,7 +6,7 @@ export let token = '';
 
 export function load(): void {
 	fs.readFile(Constants.authPath, 'utf-8', (err, data: string): void => {
-		if (err) {
+		if (err || !data) {
 			setTimeout((): void => {
 				load();
 			}, 100);
@@ -19,34 +17,6 @@ export function load(): void {
 
 		setTimeout((): void => {
 			load();
-		}, 1000);
+		}, 3000);
 	});
-
-	// return new Promise<void>((resolve, reject): void => {
-	// 	SuperAgent
-	// 		.get('/csrf')
-	// 		.set('Accept', 'application/json')
-	// 		.end((err: any, res: SuperAgent.Response): void => {
-	// 			if (res && res.status === 401) {
-	// 				window.location.href = '/login';
-	// 				resolve();
-	// 				return;
-	// 			}
-	//
-	// 			if (err) {
-	// 				reject(err);
-	// 				return;
-	// 			}
-	//
-	// 			token = res.body.token;
-	//
-	// 			if (res.body.theme === 'light') {
-	// 				Theme.light();
-	// 			} else {
-	// 				Theme.dark();
-	// 			}
-	//
-	// 			resolve();
-	// 		});
-	// });
 }
