a573056897de7b7f8ae5db4ce844b993f7feca9c
Zachary Huff <zach.huff.386@gmail.com>
2022-04-01 16:43:02 -0400

    Add tuntap win

---
+++ tuntap_win/tuntap.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+)
+
+func main() {
+	rootDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic(err)
+	}
+
+	tapInstallPath := path.Join(rootDir, "tapinstall.exe")
+
+	if os.Args[1] == "install" {
+		cmd := exec.Command(
+			tapInstallPath,
+			"install",
+			"OemVista.inf",
+			"tap0901",
+		)
+		cmd.Dir = rootDir
+
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		cmd := exec.Command(
+			tapInstallPath,
+			"remove",
+			"tap0901",
+		)
+		cmd.Dir = rootDir
+
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
