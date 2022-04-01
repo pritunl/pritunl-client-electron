d188d4ae7f77815456aab6fa2e81555c2fd4afe5
Zachary Huff <zach.huff.386@gmail.com>
2022-04-01 15:21:57 -0400

    Remove tuntap from build win

--- tools/build_win.go
+++ tools/build_win.go
@@ -31,19 +31,6 @@ func main() {
 		panic(err)
 	}
 
-	err = os.Chdir("tuntap_win")
-	if err != nil {
-		panic(err)
-	}
-
-	cmd := exec.Command("go", "build", "-v", "-o", "tuntap.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
-		panic(err)
-	}
-
 	cmd = exec.Command(signtool,
 		"sign",
 		"/a",
