a7dadf5b39e89d830a80a7335d81193c4b7a38f8
Zachary Huff <zach.huff.386@gmail.com>
2022-09-03 07:09:08 -0400

    Add log utils to app

---
+++ client/app/utils/LogUtils.ts
@@ -0,0 +1,58 @@
+/// <reference path="../References.d.ts"/>
+import * as Constants from "../Constants";
+import * as Auth from "../Auth";
+import * as Request from "../Request"
+import * as Paths from "../Paths"
+import * as MiscUtils from "./MiscUtils"
+import * as RequestUtils from "./RequestUtils"
+import * as Errors from "../Errors"
+import * as Logger from "../Logger"
+
+export async function readServiceLog(): Promise<string> {
+	let logData = ""
+
+	try {
+		let resp = await RequestUtils
+			.get('/log/service')
+			.end()
+		logData = resp.data
+	} catch (err) {
+		err = new Errors.RequestError(
+			err, "Logs: Service log request error")
+		Logger.errorAlert(err, 10)
+	}
+
+	return logData
+}
+
+export async function clearServiceLog(): Promise<void> {
+	try {
+		await RequestUtils
+			.del('/log/service')
+			.end()
+	} catch (err) {
+		err = new Errors.RequestError(
+			err, "Logs: Service log request error")
+		Logger.errorAlert(err, 10)
+	}
+}
+
+export async function readClientLog(): Promise<string> {
+	let logData = ""
+
+	try {
+		logData = await MiscUtils.fileRead(Paths.log())
+	} catch(err) {
+		Logger.errorAlert(err, 10)
+	}
+
+	return logData
+}
+
+export async function clearClientLog(): Promise<void> {
+	try {
+		await MiscUtils.fileWrite(Paths.log(), "")
+	} catch(err) {
+		Logger.errorAlert(err, 10)
+	}
+}
