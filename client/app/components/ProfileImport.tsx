5b3b5e44a1e3ce1815727c4a01f436bc3fa3ad5e
Zachary Huff <zach.huff.386@gmail.com>
2022-09-03 07:08:40 -0400

    Add profile import component

---
+++ client/app/components/ProfileImport.tsx
@@ -0,0 +1,169 @@
+/// <reference path="../References.d.ts"/>
+import * as React from "react"
+import * as Theme from "../Theme"
+import ProfilesStore from "../stores/ProfilesStore"
+import * as ProfileTypes from "../types/ProfileTypes"
+import * as ProfileActions from "../actions/ProfileActions"
+import * as ServiceActions from "../actions/ServiceActions"
+import * as Blueprint from "@blueprintjs/core"
+import PageInfo from "./PageInfo"
+import PageInput from "./PageInput"
+import PageInputFile from "./PageInputFile"
+import PageSwitch from "./PageSwitch"
+import * as Importer from "../utils/Importer"
+import path from "path"
+
+interface Props {
+	style: React.CSSProperties
+}
+
+interface State {
+	disabled: boolean
+	changed: boolean
+	dialog: boolean
+	uri: string
+	path: string
+	fullPath: string
+}
+
+const css = {
+	box: {
+		display: "inline-block"
+	} as React.CSSProperties,
+	button: {
+		marginRight: "10px",
+	} as React.CSSProperties,
+	dialog: {
+		width: "340px",
+		position: "absolute",
+	} as React.CSSProperties,
+	label: {
+		width: "100%",
+		maxWidth: "220px",
+		margin: "18px 0 0 0",
+	} as React.CSSProperties,
+	input: {
+		width: "100%",
+	} as React.CSSProperties,
+}
+
+export default class ProfileImport extends React.Component<Props, State> {
+	constructor(props: Props, context: any) {
+		super(props, context)
+		this.state = {
+			disabled: false,
+			changed: false,
+			dialog: false,
+			uri: "",
+			path: "",
+			fullPath: "",
+		}
+	}
+
+	onImportUri = (): void => {
+		this.setState({
+			...this.state,
+			disabled: true,
+		})
+
+		Importer.importUri(this.state.uri).then(() => {
+			this.setState({
+				...this.state,
+				dialog: false,
+				disabled: false,
+				changed: false,
+				uri: "",
+				path: "",
+				fullPath: "",
+			})
+		})
+	}
+
+	openDialog = (): void => {
+		this.setState({
+			...this.state,
+			dialog: true,
+		})
+	}
+
+	closeDialog = (): void => {
+		this.setState({
+			...this.state,
+			dialog: false,
+		})
+	}
+
+	render(): JSX.Element {
+		return <div style={css.box}>
+			<button
+				className="bp3-button bp3-minimal bp3-icon-import"
+				style={this.props.style}
+				type="button"
+				disabled={this.state.disabled}
+				onClick={this.openDialog}
+			>
+				Import
+			</button>
+			<Blueprint.Dialog
+				title="Import Profile"
+				style={css.dialog}
+				isOpen={this.state.dialog}
+				usePortal={true}
+				portalContainer={document.body}
+				onClose={this.closeDialog}
+			>
+				<div className="bp3-dialog-body">
+					<PageInput
+						disabled={this.state.disabled}
+						label="Profile URI"
+						help="Profile URI as shown in the Pritunl server web console."
+						type="text"
+						placeholder="Enter URI"
+						value={this.state.uri}
+						onChange={(val: string): void => {
+							this.setState({
+								...this.state,
+								changed: true,
+								uri: val,
+								path: "",
+								fullPath: "",
+							})
+						}}
+					/>
+					<PageInputFile
+						disabled={this.state.disabled}
+						label="Import Profile"
+						help="Select profile file in tar, zip, ovpn or conf format."
+						accept=".ovpn,.conf,.tar,.zip"
+						value={this.state.path}
+						onChange={(val: string): void => {
+							this.setState({
+								...this.state,
+								changed: true,
+								uri: "",
+								path: path.basename(val),
+								fullPath: val,
+							})
+						}}
+					/>
+				</div>
+				<div className="bp3-dialog-footer">
+					<div className="bp3-dialog-footer-actions">
+						<button
+							className="bp3-button bp3-intent-danger bp3-icon-cross"
+							type="button"
+							disabled={this.state.disabled}
+							onClick={this.closeDialog}
+						>Cancel</button>
+						<button
+							className="bp3-button bp3-intent-success bp3-icon-tick"
+							type="button"
+							disabled={this.state.disabled || !this.state.changed}
+							onClick={this.onImportUri}
+						>Import</button>
+					</div>
+				</div>
+			</Blueprint.Dialog>
+		</div>
+	}
+}
