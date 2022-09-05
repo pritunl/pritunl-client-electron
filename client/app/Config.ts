9993a19a4213ead55b1c5f92a392abcfb142ac4a
Zachary Huff <zach.huff.386@gmail.com>
2022-09-05 10:52:08 -0400

    Add window size to app config

--- client/app/Config.ts
+++ client/app/Config.ts
@@ -1,13 +1,15 @@
 /// <reference path="./References.d.ts"/>
-import * as Constants from "./Constants";
-import * as Errors from "./Errors";
-import * as Logger from "./Logger";
-import * as Paths from "./Paths";
-import fs from "fs";
+import * as Errors from "./Errors"
+import * as Logger from "./Logger"
+import * as Paths from "./Paths"
+import fs from "fs"
 
 class ConfigData {
-	disable_tray_icon = false;
-	theme = "dark";
+	window_width = 0
+	window_height = 0
+	disable_tray_icon = false
+	classic_interface = false
+	theme = "dark"
 
 	load(): Promise<void> {
 		return new Promise<void>((resolve, reject): void => {
@@ -16,35 +18,44 @@ class ConfigData {
 				(err: NodeJS.ErrnoException, data: string): void => {
 					if (err) {
 						if (err.code !== "ENOENT") {
-							err = new Errors.ReadError(err, "Config: Read error");
-							Logger.errorAlert(err, 10);
+							err = new Errors.ReadError(err, "Config: Read error")
+							Logger.errorAlert(err, 10)
 						}
 
-						resolve();
-						return;
+						resolve()
+						return
 					}
 
-					let configData: any;
+					let configData: any
 					try {
-						configData = JSON.parse(data);
+						configData = JSON.parse(data)
 					} catch (err) {
-						err = new Errors.ReadError(err, "Config: Parse error");
-						Logger.errorAlert(err, 10);
+						err = new Errors.ReadError(err, "Config: Parse error")
+						Logger.errorAlert(err, 10)
 
-						configData = {};
+						configData = {}
 					}
 
 					if (configData["disable_tray_icon"] !== undefined) {
-						this.disable_tray_icon = configData["disable_tray_icon"];
+						this.disable_tray_icon = configData["disable_tray_icon"]
+					}
+					if (configData["classic_interface"] !== undefined) {
+						this.classic_interface = configData["classic_interface"]
 					}
 					if (configData["theme"] !== undefined) {
-						this.theme = configData["theme"];
+						this.theme = configData["theme"]
+					}
+					if (configData["window_width"] !== undefined) {
+						this.window_width = configData["window_width"]
+					}
+					if (configData["window_height"] !== undefined) {
+						this.window_height = configData["window_height"]
 					}
 
-					resolve();
+					resolve()
 				},
-			);
-		});
+			)
+		})
 	}
 
 	save(): Promise<void> {
@@ -52,20 +63,23 @@ class ConfigData {
 			fs.writeFile(
 				Paths.config(), JSON.stringify({
 					disable_tray_icon: this.disable_tray_icon,
+					classic_interface: this.classic_interface,
+					window_width: this.window_width,
+					window_height: this.window_height,
 					theme: this.theme,
 				}),
 				(err: NodeJS.ErrnoException): void => {
 					if (err) {
-						err = new Errors.ReadError(err, "Config: Write error");
-						Logger.errorAlert(err);
+						err = new Errors.ReadError(err, "Config: Write error")
+						Logger.errorAlert(err)
 					}
 
-					resolve();
+					resolve()
 				},
-			);
-		});
+			)
+		})
 	}
 }
 
-const Config = new ConfigData();
-export default Config;
+const Config = new ConfigData()
+export default Config
