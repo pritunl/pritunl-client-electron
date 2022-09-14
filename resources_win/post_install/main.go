c80f6633c0576df7134dad33fb020b3e30fb9cf1
Zachary Huff <zach.huff.386@gmail.com>
2022-09-13 22:32:20 -0400

    Update win post install

--- resources_win/post_install/main.go
+++ resources_win/post_install/main.go
@@ -7,14 +7,6 @@ import (
 	"path/filepath"
 )
 
-func GetWinDrive() string {
-	systemDrv := os.Getenv("SYSTEMDRIVE")
-	if systemDrv == "" {
-		return "C:\\"
-	}
-	return systemDrv + "\\"
-}
-
 func main() {
 	rootDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
@@ -25,28 +17,8 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "pritunl-service.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "openvpn.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
+	cmd = exec.Command(filepath.Join(rootDir, "sc.exe"),
 		"stop", "pritunl")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "pritunl.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "pritunl-service.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "openvpn.exe")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
@@ -62,67 +34,30 @@ func main() {
 	cmd.Stderr = os.Stderr
 	cmd.Run()
 
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
+	cmd = exec.Command(filepath.Join(rootDir, "sc.exe"),
 		"stop", "pritunl")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"remove", "pritunl", "confirm")
+	cmd = exec.Command(filepath.Join(rootDir, "sc.exe"),
+		"delete", "pritunl")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"stop", "pritunl")
+
+	cmd = exec.Command(
+		filepath.Join(rootDir, "sc.exe"),
+		"create", "pritunl",
+		"start=auto",
+		`displayname="Pritunl Client Helper Service"`,
+		fmt.Sprintf(`binpath="%s"`,
+			filepath.Join(rootDir, "pritunl-service.exe")),
+	)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "pritunl.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command("taskkill.exe", "/F", "/IM", "pritunl-service.exe")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"), "install",
-		"pritunl", filepath.Join(rootDir, "pritunl-service.exe"))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command("sc.exe", "config", "pritunl",
-		fmt.Sprintf(`binPath="%s"`, filepath.Join(rootDir, "nssm.exe")))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"set", "pritunl", "DisplayName", "Pritunl Helper Service")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"set", "pritunl", "Start", "SERVICE_AUTO_START")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"set", "pritunl", "AppStdout",
-		filepath.Join(GetWinDrive(), "ProgramData", "Pritunl", "service.log"))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"set", "pritunl", "AppStderr",
-		filepath.Join(GetWinDrive(), "ProgramData", "Pritunl", "service.log"))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
-		"set", "pritunl", "Start", "SERVICE_AUTO_START")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	cmd = exec.Command(filepath.Join(rootDir, "nssm.exe"),
+
+	cmd = exec.Command(filepath.Join(rootDir, "sc.exe"),
 		"start", "pritunl")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
