200742c0cb5b2c563a482c572aa9b393280f4dee
Zachary Huff <zach.huff.386@gmail.com>
2022-09-16 12:20:04 -0400

    Change info layout in client profile component

--- client/app/components/Profile.tsx
+++ client/app/components/Profile.tsx
@@ -8,6 +8,7 @@ import * as ServiceActions from '../actions/ServiceActions';
 import * as Constants from "../Constants";
 import * as MiscUtils from "../utils/MiscUtils";
 import * as Blueprint from "@blueprintjs/core";
+import * as PageInfos from './PageInfo';
 import ConfirmButton from "./ConfirmButton";
 import PageInfo from './PageInfo';
 import PageSwitch from './PageSwitch';
@@ -58,13 +59,30 @@ const css = {
 	} as React.CSSProperties,
 	deleteButtonBox: {
 		position: "absolute",
-		top: "5px",
-		right: "5px",
+		top: "4px",
+		right: "4px",
 	} as React.CSSProperties,
 	buttons: {
 	} as React.CSSProperties,
 	editor: {
 		margin: '10px 0 0 0',
+	} as React.CSSProperties,
+	header: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		padding: '4px',
+		height: '39px',
+	} as React.CSSProperties,
+	headerLabel: {
+		fontSize: "1.09em",
+		margin: "5px 34px 0 6px",
+		overflow: "hidden",
+		whiteSpace: "nowrap",
+	} as React.CSSProperties,
+	body: {
+		paddingTop: "31px"
 	} as React.CSSProperties,
 };
 
@@ -117,8 +135,6 @@ export default class Profile extends React.Component<Props, State> {
 		let profile: ProfileTypes.Profile = this.state.profile ||
 			this.props.profile;
 
-		let syncHosts = profile.formatedHosts();
-
 		let statusLabel = "Online For"
 		let statusVal = profile.formattedUptime()
 		if (statusVal === "") {
@@ -126,92 +142,93 @@ export default class Profile extends React.Component<Props, State> {
 			statusVal = profile.formattedStatus()
 		}
 
-		let lastSync = ""
-		if (profile.sync_time) {
-			lastSync = MiscUtils.formatDateLess(profile.sync_time)
-		} else {
-			lastSync = "Never"
-		}
-		syncHosts.push('Last Sync: ' + lastSync)
+		let fieldsLeft: PageInfos.Field[] = [
+			{
+				label: 'User',
+				value: profile.user || '-',
+			},
+			{
+				label: 'Server',
+				value: profile.server || '-',
+			},
+		]
 
-		return <div className="bp3-card" style={css.card}>
-			<div style={css.deleteButtonBox}>
-				<ConfirmButton
-					className="bp3-minimal bp3-intent-danger bp3-icon-trash"
-					style={css.deleteButton}
-					safe={true}
-					progressClassName="bp3-intent-danger"
-					dialogClassName="bp3-intent-danger bp3-icon-delete"
-					dialogLabel="Delete Profile"
-					confirmMsg="Permanently delete this profile"
-					items={[profile.formattedName()]}
-					disabled={this.state.disabled}
-					onConfirm={this.onDelete}
-				/>
+		let fieldsRight: PageInfos.Field[] = [
+			{
+				label: statusLabel,
+				value: statusVal,
+			},
+			{
+				label: 'Organization',
+				value: profile.organization || '-',
+			},
+		]
+
+		let fieldsLong: PageInfos.Field[] = []
+
+		let longIp = false
+		if ((profile.server_addr && profile.server_addr.length >= 16) ||
+			(profile.client_addr && profile.client_addr.length >= 16)) {
+
+			fieldsLong.push({
+				label: 'Server Address',
+				value: profile.server_addr || '-',
+				copy: !!profile.server_addr,
+			})
+			fieldsLong.push({
+				label: 'Client Address',
+				value: profile.client_addr || '-',
+				copy: !!profile.client_addr,
+			})
+
+			longIp = true
+		} else if (profile.server_addr || profile.client_addr) {
+			fieldsLeft.push({
+				label: 'Server Address',
+				value: profile.server_addr || '-',
+				copy: !!profile.server_addr,
+			})
+			fieldsRight.push({
+				label: 'Client Address',
+				value: profile.client_addr || '-',
+				copy: !!profile.client_addr,
+			})
+		}
+
+		return <div className="bp3-card layout vertical" style={css.card}>
+			<div className="bp3-card-header" style={css.header}>
+				<h3
+					style={css.headerLabel}
+				>{profile.formattedName() || 'Profile'}</h3>
+				<div style={css.deleteButtonBox}>
+					<ConfirmButton
+						className="bp3-minimal bp3-intent-danger bp3-icon-trash"
+						style={css.deleteButton}
+						safe={true}
+						progressClassName="bp3-intent-danger"
+						dialogClassName="bp3-intent-danger bp3-icon-delete"
+						dialogLabel="Delete Profile"
+						confirmMsg="Permanently delete this profile"
+						items={[profile.formattedName()]}
+						disabled={this.state.disabled}
+						onConfirm={this.onDelete}
+					/>
+				</div>
 			</div>
-			<div className="layout horizontal">
+			<div className="layout horizontal" style={css.body}>
 				<PageInfo
 					style={css.label}
-					fields={[
-						{
-							label: 'ID',
-							value: profile.id.substring(0, 18) || '-',
-						},
-						{
-							label: 'Name',
-							value: profile.formattedName() || '-',
-						},
-						{
-							label: 'User',
-							value: profile.user || '-',
-						},
-						{
-							label: 'Server',
-							value: profile.server || '-',
-						},
-					]}
+					fields={fieldsLeft}
 				/>
 				<PageInfo
 					style={css.label}
-					fields={[
-						{
-							label: statusLabel,
-							value: statusVal,
-						},
-						{
-							label: 'Organization',
-							value: profile.organization || '-',
-						},
-						{
-							label: 'Type',
-							value: profile.system ? 'System' : 'User',
-						},
-						{
-							label: 'Autostart',
-							value: (profile.system &&
-								!profile.disabled) ? 'Enabled' : 'Disabled',
-						},
-					]}
+					fields={fieldsRight}
 				/>
 			</div>
 			<PageInfo
 				style={css.labelLast}
-				fields={[
-					{
-						label: 'Server Address',
-						value: profile.server_addr || '-',
-						copy: !!profile.server_addr,
-					},
-					{
-						label: 'Client Address',
-						value: profile.client_addr || '-',
-						copy: !!profile.client_addr,
-					},
-					{
-						label: 'Configuration Sync Hosts',
-						value: syncHosts,
-					},
-				]}
+				hidden={!longIp}
+				fields={fieldsLong}
 			/>
 			<div>
 				<PageSwitch
