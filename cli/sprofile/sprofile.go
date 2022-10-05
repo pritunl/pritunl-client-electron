2b9faa0f76d5dc07f9707b6b9a9831cc2fe639af
Zachary Huff <zach.huff.386@gmail.com>
2022-10-04 20:49:06 -0400

    Add sso auth to cli sprofile

--- cli/sprofile/sprofile.go
+++ cli/sprofile/sprofile.go
@@ -27,6 +27,7 @@ type Sprofile struct {
 	User               string           `json:"user"`
 	PreConnectMsg      string           `json:"pre_connect_msg"`
 	DynamicFirewall    bool             `json:"dynamic_firewall"`
+	SsoAuth            bool             `json:"sso_auth"`
 	PasswordMode       string           `json:"password_mode"`
 	Token              bool             `json:"token"`
 	TokenTtl           int              `json:"token_ttl"`
