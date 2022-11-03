7e1bf718e32d7d1f5cd53b33d7a11442b2ec46af
Zachary Huff <zach.huff.386@gmail.com>
2022-11-03 17:38:31 -0400

    Set reconnect in profile connect component

--- client/app/components/ProfileConnect.tsx
+++ client/app/components/ProfileConnect.tsx
@@ -259,6 +259,7 @@ export default class ProfileConnect extends React.Component<Props, State> {
 			server_box_public_key: prfl.server_box_public_key,
 			token_ttl: prfl.token_ttl,
 			timeout: true,
+			reconnect: !prfl.disable_reconnect,
 			data: data,
 		}
 
