64f04dfecc1036c3772da23f06b27d371f98c3d2
Zachary Huff <zach.huff.386@gmail.com>
2023-02-12 21:01:05 -0500

    Add exec input logged to service proc utils

--- service/utils/proc.go
+++ service/utils/proc.go
@@ -32,6 +32,7 @@ func Exec(name string, arg ...string) (err error) {
 
 func ExecInput(dir, input, name string, arg ...string) (err error) {
 	cmd := command.Command(name, arg...)
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -56,23 +57,33 @@ func ExecInput(dir, input, name string, arg ...string) (err error) {
 		return
 	}
 
-	_, err = io.WriteString(stdin, input)
-	if err != nil {
-		err = &errortypes.ExecError{
-			errors.Wrapf(err, "utils: Failed to write stdin in exec '%s'",
-				name),
-		}
-		return
-	}
+	var wrErr error
+	go func() {
+		defer func() {
+			wrErr = stdin.Close()
+			if wrErr != nil {
+				wrErr = &errortypes.ExecError{
+					errors.Wrapf(
+						wrErr,
+						"utils: Failed to close stdin in exec '%s'",
+						name,
+					),
+				}
+			}
+		}()
 
-	err = stdin.Close()
-	if err != nil {
-		err = &errortypes.ExecError{
-			errors.Wrapf(err, "utils: Failed to close stdin in exec '%s'",
-				name),
+		_, wrErr = io.WriteString(stdin, input)
+		if wrErr != nil {
+			wrErr = &errortypes.ExecError{
+				errors.Wrapf(
+					wrErr,
+					"utils: Failed to write stdin in exec '%s'",
+					name,
+				),
+			}
+			return
 		}
-		return
-	}
+	}()
 
 	err = cmd.Wait()
 	if err != nil {
@@ -82,16 +93,21 @@ func ExecInput(dir, input, name string, arg ...string) (err error) {
 		return
 	}
 
+	if wrErr != nil {
+		return
+	}
+
 	return
 }
 
 func ExecInputOutput(input, name string, arg ...string) (
 	output string, err error) {
 
-	var stdout bytes.Buffer
-
 	cmd := command.Command(name, arg...)
-	cmd.Stdout = &stdout
+
+	stdout := &bytes.Buffer{}
+
+	cmd.Stdout = stdout
 	cmd.Stderr = os.Stderr
 
 	stdin, err := cmd.StdinPipe()
@@ -111,22 +127,137 @@ func ExecInputOutput(input, name string, arg ...string) (
 		return
 	}
 
-	_, err = io.WriteString(stdin, input)
-	if err != nil {
-		stdin.Close()
-		err = &errortypes.ExecError{
-			errors.Wrapf(err, "utils: Failed to write stdin in exec '%s'",
-				name),
+	var wrErr error
+	go func() {
+		defer func() {
+			wrErr = stdin.Close()
+			if wrErr != nil {
+				wrErr = &errortypes.ExecError{
+					errors.Wrapf(
+						wrErr,
+						"utils: Failed to close stdin in exec '%s'",
+						name,
+					),
+				}
+			}
+		}()
+
+		_, wrErr = io.WriteString(stdin, input)
+		if wrErr != nil {
+			wrErr = &errortypes.ExecError{
+				errors.Wrapf(
+					wrErr,
+					"utils: Failed to write stdin in exec '%s'",
+					name,
+				),
+			}
+			return
 		}
-		return
-	}
-	stdin.Close()
+	}()
 
 	err = cmd.Wait()
 	if err != nil {
 		err = &errortypes.ExecError{
 			errors.Wrapf(err, "utils: Failed to exec '%s'", name),
 		}
+		return
+	}
+
+	if wrErr != nil {
+		return
+	}
+
+	output = string(stdout.Bytes())
+
+	return
+}
+
+func ExecInputOutputCombindLogged(input, name string, arg ...string) (
+	output string, err error) {
+
+	cmd := command.Command(name, arg...)
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		err = &errortypes.ExecError{
+			errors.Wrapf(err, "utils: Failed to get stdin in exec '%s'", name),
+		}
+		return
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		stdin.Close()
+		err = &errortypes.ExecError{
+			errors.Wrapf(err, "utils: Failed to exec '%s'", name),
+		}
+		return
+	}
+
+	var wrErr error
+	go func() {
+		defer func() {
+			wrErr = stdin.Close()
+			if wrErr != nil {
+				wrErr = &errortypes.ExecError{
+					errors.Wrapf(
+						wrErr,
+						"utils: Failed to close stdin in exec '%s'",
+						name,
+					),
+				}
+			}
+		}()
+
+		_, wrErr = io.WriteString(stdin, input)
+		if wrErr != nil {
+			wrErr = &errortypes.ExecError{
+				errors.Wrapf(
+					wrErr,
+					"utils: Failed to write stdin in exec '%s'",
+					name,
+				),
+			}
+			return
+		}
+	}()
+
+	err = cmd.Wait()
+
+	output = stdout.String()
+	errOutput := stderr.String()
+
+	if err != nil {
+		err = &errortypes.ExecError{
+			errors.Wrapf(err, "utils: Failed to exec '%s'", name),
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"output":       output,
+			"error_output": errOutput,
+			"cmd":          name,
+			"arg":          arg,
+			"error":        err,
+		}).Error("utils: Process exec error")
+
+		return
+	}
+
+	if wrErr != nil {
+		logrus.WithFields(logrus.Fields{
+			"output":       output,
+			"error_output": errOutput,
+			"cmd":          name,
+			"arg":          arg,
+			"error":        wrErr,
+		}).Error("utils: Process exec error")
+
 		return
 	}
 
