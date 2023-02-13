b7ee14cf708ce9eb8047cb4f5d7b96a1560558cf
Zachary Huff <zach.huff.386@gmail.com>
2023-02-12 21:15:44 -0500

    Add config types to app

---
+++ client/app/types/ConfigTypes.ts
@@ -0,0 +1,17 @@
+/// <reference path="../References.d.ts"/>
+export const SYNC = 'config.sync';
+export const CHANGE = 'config.change';
+
+export interface Config {
+	disable_dns_watch?: boolean
+	disable_wake_watch?: boolean
+	disable_net_clean?: boolean
+	interface_metric?: number
+}
+
+export type ConfigRo = Readonly<Config>;
+
+export interface ConfigDispatch {
+	type: string;
+	data?: Config;
+}
