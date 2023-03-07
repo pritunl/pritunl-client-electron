bc246dcb1c7702f2cae93d9e25fb2990e01fc286
Zachary Huff <zach.huff.386@gmail.com>
2023-03-07 11:42:58 -0500

    Add error logging to client app

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -9,6 +9,40 @@ import * as Auth from "./Auth";
 import * as Theme from "./Theme";
 import * as Constants from "./Constants";
 import Config from "./Config";
+import * as Errors from "./Errors";
+import * as Logger from "./Logger";
+
+window.onerror = (event, source, line, col, err): void => {
+	err = new Errors.UnknownError(err, "Main: Unhandled exception", {
+		event: event,
+		source: source,
+		line: line,
+		column: col,
+	})
+	Logger.errorAlert(err)
+}
+
+window.onunhandledrejection = (event): void => {
+	let message = ""
+	let stack = ""
+
+	try {
+		message = event.reason.message
+	} catch {
+		message = event.reason
+	}
+
+	try {
+		stack = event.reason.stack
+	} catch {
+	}
+
+	let err = new Errors.UnknownError(null, "Main: Unhandled rejection", {
+		message: message,
+		stack: stack,
+	})
+	Logger.errorAlert(err)
+}
 
 Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
 Alert.init();
