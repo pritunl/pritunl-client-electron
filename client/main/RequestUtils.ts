5b881b54338e27b52fbefd3578ebcddcc97a6662
Zachary Huff <zach.huff.386@gmail.com>
2024-04-12 23:18:35 -0400

    Remove unused func in client request utils

--- client/main/RequestUtils.ts
+++ client/main/RequestUtils.ts
@@ -1,4 +1,3 @@
-/// <reference path="../References.d.ts"/>
 import * as Auth from "./Auth";
 import * as Request from "./Request"
 import crypto from "crypto";
@@ -65,29 +64,4 @@ export function del(path: string): Request.Request {
 		.set("User-Agent", "pritunl")
 
 	return req
-}
-
-export function authGet(host: string, path: string,
-	token: string, secret: string): Request.Request {
-
-	let req = new Request.Request()
-
-	req.get(host + path)
-		.set("Auth-Token", Auth.token)
-		.set("User-Agent", "pritunl")
-
-	let authTimestamp = Math.floor(new Date().getTime() / 1000).toString()
-	let authNonce = MiscUtils.nonce()
-	let authString = [token, authTimestamp, authNonce, "get", path].join("&")
-	let authSignature = crypto.createHmac("sha512", secret).update(
-		authString).digest("base64")
-
-	req.secure(false)
-		.set("Auth-Token", token)
-		.set("Auth-Timestamp", authTimestamp)
-		.set("Auth-Nonce", authNonce)
-		.set("Auth-Signature", authSignature)
-
-	return req
-
 }
