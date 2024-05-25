88a7fad24365f60ac4281967689b59dee024b7fd
Zachary Huff <zach.huff.386@gmail.com>
2024-05-24 23:18:47 -0400

    Add uuid to client utils

--- client/main/Utils.ts
+++ client/main/Utils.ts
@@ -2,6 +2,21 @@ import electron from "electron"
 import process from "process";
 import childprocess from "child_process";
 
+export function uuid(): string {
+	return (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+}
+
+export function uuidRand(): string {
+	let id = ""
+
+	for (let i = 0; i < 4; i++) {
+		id += Math.floor((1 + Math.random()) * 0x10000).toString(
+			16).substring(1);
+	}
+
+	return id;
+}
+
 export function openLink(url: string): boolean {
 	let u = new URL(url)
 
