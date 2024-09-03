16ca083cd5cb2e954f0e1eb5ea1b36ff9e32e90b
Zachary Huff <zach.huff.386@gmail.com>
2024-09-02 23:35:20 -0400

    Add get sso url to cli profile utils

--- cli/profile/utils.go
+++ cli/profile/utils.go
@@ -10,6 +10,55 @@ import (
 	"github.com/pritunl/pritunl-client-electron/cli/service"
 )
 
+func Get(prflId string) (prfl *Profile, err error) {
+	reqUrl := service.GetAddress() + "/profile/" + prflId
+
+	authKey, err := service.GetAuthKey()
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "profile: Get request failed"),
+		}
+		return
+	}
+
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		req.Host = "unix"
+	}
+	req.Header.Set("Auth-Key", authKey)
+	req.Header.Set("User-Agent", "pritunl")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := service.GetClient().Do(req)
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "profile: Request failed"),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		prfl = nil
+		return
+	}
+
+	prfl = &Profile{}
+	err = json.NewDecoder(resp.Body).Decode(&prfl)
+	if err != nil {
+		err = errortypes.ParseError{
+			errors.Wrap(err, "profile: Failed to parse response"),
+		}
+		return
+	}
+
+	return
+}
+
 func GetAll() (prfls map[string]*Profile, err error) {
 	reqUrl := service.GetAddress() + "/profile"
 
