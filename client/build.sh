9803b18f87e2951e952c309ea64d055e1a68c267
Zachary Huff <zach.huff.386@gmail.com>
2024-09-15 04:50:39 -0400

    Update client build

--- client/build.sh
+++ client/build.sh
@@ -3,19 +3,23 @@ tsc
 # development
 rm -rf dist-dev/static
 mkdir -p dist-dev/static
-cp styles/global.css dist-dev/static/
 cp styles/fredoka-one.ttf dist-dev/static/
+cp styles/global.css dist-dev/static/
+cp styles/blueprint.css dist-dev/static/
 cp node_modules/normalize.css/normalize.css dist-dev/static/
-cp node_modules/@blueprintjs/core/lib/css/blueprint.css dist-dev/static/
-cp node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css dist-dev/static/
+cp node_modules/@blueprintjs/datetime2/lib/css/blueprint-datetime2.css dist-dev/static/
 cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css dist-dev/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-16.eot dist-dev/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-16.ttf dist-dev/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist-dev/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist-dev/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist-dev/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist-dev/static/
-cp node_modules/source-map/lib/mappings.wasm dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.eot dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.svg dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.ttf dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.woff dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.woff2 dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.eot dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.svg dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.ttf dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.woff dist-dev/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.woff2 dist-dev/static/
+cp -r node_modules/monaco-editor/min/vs dist-dev/static/
 sed -i 's|../../resources/icons/||g' dist-dev/static/blueprint-icons.css
 
 webpack --config webpack.dev.config
@@ -26,19 +30,23 @@ cp index.html dist-dev/index.html
 # production
 rm -rf dist/static
 mkdir -p dist/static
-cp styles/global.css dist/static/
 cp styles/fredoka-one.ttf dist/static/
+cp styles/global.css dist/static/
+cp styles/blueprint.css dist/static/
 cp node_modules/normalize.css/normalize.css dist/static/
-cp node_modules/@blueprintjs/core/lib/css/blueprint.css dist/static/
-cp node_modules/@blueprintjs/datetime/lib/css/blueprint-datetime.css dist/static/
+cp node_modules/@blueprintjs/datetime2/lib/css/blueprint-datetime2.css dist/static/
 cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css dist/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-16.eot dist/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-16.ttf dist/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-16.woff dist/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-20.eot dist/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-20.ttf dist/static/
-cp node_modules/@blueprintjs/icons/resources/icons/icons-20.woff dist/static/
-cp node_modules/source-map/lib/mappings.wasm dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.eot dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.svg dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.ttf dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.woff dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-16.woff2 dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.eot dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.svg dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.ttf dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.woff dist/static/
+cp node_modules/@blueprintjs/icons/lib/css/blueprint-icons-20.woff2 dist/static/
+cp -r node_modules/monaco-editor/min/vs dist/static/
 sed -i 's|../../resources/icons/||g' dist/static/blueprint-icons.css
 
 webpack --config webpack.config
