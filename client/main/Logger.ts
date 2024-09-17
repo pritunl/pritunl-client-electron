5fbf58d25550bdcce69d7210f729a6845135ce11
Zachary Huff <zach.huff.386@gmail.com>
2024-09-16 21:08:46 -0400

    Update logger in client main

--- client/main/Logger.ts
+++ client/main/Logger.ts
@@ -2,33 +2,55 @@ import fs from "fs"
 import path from "path"
 import electron from "electron"
 import * as Errors from "./Errors"
+import * as Constants from "./Constants";
 
-function push(level: string, msg: string): void {
+function push(level: string, err: any): void {
+	if (!err) {
+		err = "Undefined error"
+	}
+
 	let time = new Date()
+	let msg = err.message || err
+
 	msg = "[" + time.getFullYear() + "-" + (time.getMonth() + 1) + "-" +
 		time.getDate() + " " + time.getHours() + ":" + time.getMinutes() + ":" +
-		time.getSeconds() + "][" + level  + "] " + msg + "\n"
+		time.getSeconds() + "][" + level  + "] " + msg + "\n" + (err.stack || "")
 
-	console.error(msg)
+	msg = msg.trim()
 
-	let logPath = path.join(electron.app.getPath("userData"), "pritunl.log")
+	let pth = Constants.logPath
 
-	fs.appendFile(logPath, msg, (err: Error): void => {
-		if (err) {
-			err = new Errors.WriteError(err, "Logger: Failed to write log")
-			console.error(err)
+	fs.stat(pth, (err: Error, stat) => {
+		if (stat && stat.size > 200000) {
+			fs.unlink(pth, () => {
+				fs.appendFile(pth, msg + "\n", (err: Error): void => {
+					if (err) {
+						err = new Errors.WriteError(err, "Logger: Failed to write log",
+							{log_path: pth})
+						console.error(err)
+					}
+				})
+			})
+		} else {
+			fs.appendFile(pth, msg + "\n", (err: Error): void => {
+				if (err) {
+					err = new Errors.WriteError(err, "Logger: Failed to write log",
+						{log_path: pth})
+					console.error(err)
+				}
+			})
 		}
 	})
 }
 
-export function info(msg: string): void {
-	push("INFO", msg)
+export function info(err: any): void {
+	push("INFO", err)
 }
 
-export function warning(msg: string): void {
-	push("WARN", msg)
+export function warning(err: any): void {
+	push("WARN", err)
 }
 
-export function error(msg: string): void {
-	push("ERROR", msg)
+export function error(err: any): void {
+	push("ERROR", err)
 }
