66476309c5a66558810e0d0964f087487c6eed2e
Zachary Huff <zach.huff.386@gmail.com>
2024-10-06 15:36:05 -0400

    Add connection profile

---
+++ service/connection/profile.go
@@ -0,0 +1,129 @@
+package connection
+
+import (
+	"context"
+	"strings"
+	"sync"
+
+	"github.com/pritunl/pritunl-client-electron/service/sprofile"
+	"github.com/sirupsen/logrus"
+)
+
+type Profile struct {
+	conn               *Connection        `json:"-"`
+	Id                 string             `json:"id"`
+	Mode               string             `json:"mode"`
+	OrgId              string             `json:"org_id"`
+	UserId             string             `json:"user_id"`
+	ServerId           string             `json:"server_id"`
+	SyncHosts          []string           `json:"sync_hosts"`
+	SyncToken          string             `json:"sync_token"`
+	SyncSecret         string             `json:"sync_secret"`
+	Data               string             `json:"data"`
+	Username           string             `json:"username"`
+	Password           string             `json:"password"`
+	DynamicFirewall    bool               `json:"dynamic_firewall"`
+	GeoSort            string             `json:"geo_sort"`
+	ForceConnect       bool               `json:"force_connect"`
+	DeviceAuth         bool               `json:"device_auth"`
+	DisableGateway     bool               `json:"disable_gateway"`
+	DisableDns         bool               `json:"disable_dns"`
+	RestrictClient     bool               `json:"restrict_client"`
+	ForceDns           bool               `json:"force_dns"`
+	SsoAuth            bool               `json:"sso_auth"`
+	ServerPublicKey    string             `json:"server_public_key"`
+	ServerBoxPublicKey string             `json:"server_box_public_key"`
+	TokenTtl           int                `json:"token_ttl"`
+	Reconnect          bool               `json:"reconnect"`
+	Timeout            bool               `json:"timeout"`
+	SystemProfile      bool               `json:"-"`
+	requestCancelLock  sync.Mutex         `json:"-"`
+	requestCancel      context.CancelFunc `json:"-"`
+}
+
+func (p *Profile) Fields() logrus.Fields {
+	return logrus.Fields{
+		"profile_id":               p.Id,
+		"profile_mode":             p.Mode,
+		"profile_dynamic_firewall": p.DynamicFirewall,
+		"profile_device_auth":      p.DeviceAuth,
+		"profile_disable_gateway":  p.DisableGateway,
+		"profile_disable_dns":      p.DisableDns,
+		"profile_geo_sort":         p.IsGeoSort(),
+		"profile_force_connect":    p.ForceConnect,
+		"profile_force_dns":        p.ForceDns,
+		"profile_sso_auth":         p.SsoAuth,
+		"profile_reconnect":        p.Reconnect,
+		"profile_timeout":          p.Timeout,
+	}
+}
+
+func (p *Profile) IsGeoSort() bool {
+	return p.GeoSort != ""
+}
+
+func (p *Profile) Sync() {
+	if !p.SystemProfile {
+		sprfl := sprofile.Get(p.Id)
+		if sprfl == nil {
+			logrus.WithFields(p.conn.Fields(nil)).Error(
+				"profile: Missing system profile in sync")
+			return
+		}
+
+		updated, err := sprfl.Sync()
+		if err != nil {
+			logrus.WithFields(p.conn.Fields(logrus.Fields{
+				"error": err,
+			})).Error("profile: Failed to sync system profile")
+			sprfl.SyncTime = -1
+			sprfl.Commit()
+		}
+
+		if updated {
+			p.ImportSystemProfile(sprfl)
+		}
+	} else {
+		// TODO Sync non system profiles
+	}
+
+	return
+}
+
+func (p *Profile) ImportSystemProfile(sprfl *sprofile.Sprofile) {
+	serverPublicKey := ""
+	if sprfl.ServerPublicKey != nil && len(sprfl.ServerPublicKey) > 0 {
+		serverPublicKey = strings.Join(sprfl.ServerPublicKey, "\n")
+	}
+
+	lastMode := sprfl.LastMode
+	if lastMode == "" {
+		lastMode = OvpnMode
+	}
+
+	p.Id = sprfl.Id
+	p.Mode = lastMode
+	p.OrgId = sprfl.OrganizationId
+	p.UserId = sprfl.UserId
+	p.ServerId = sprfl.ServerId
+	p.SyncHosts = sprfl.SyncHosts
+	p.SyncToken = sprfl.SyncToken
+	p.SyncSecret = sprfl.SyncSecret
+	p.Data = sprfl.OvpnData
+	p.Username = "pritunl"
+	p.Password = sprfl.Password
+	p.DynamicFirewall = sprfl.DynamicFirewall
+	p.GeoSort = sprfl.GeoSort
+	p.ForceConnect = sprfl.ForceConnect
+	p.DeviceAuth = sprfl.DeviceAuth
+	p.DisableGateway = sprfl.DisableGateway
+	p.DisableDns = sprfl.DisableDns
+	p.RestrictClient = sprfl.RestrictClient
+	p.ForceDns = sprfl.ForceDns
+	p.SsoAuth = sprfl.SsoAuth
+	p.ServerPublicKey = serverPublicKey
+	p.ServerBoxPublicKey = sprfl.ServerBoxPublicKey
+	p.TokenTtl = sprfl.TokenTtl
+	p.Reconnect = true
+	p.SystemProfile = true
+}
