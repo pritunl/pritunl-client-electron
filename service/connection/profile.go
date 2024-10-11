d276a60b2d728ce8a6e563595fc4e0130966e4f2
Zachary Huff <zach.huff.386@gmail.com>
2024-10-11 13:36:40 -0400

    Update service connection profile

--- service/connection/profile.go
+++ service/connection/profile.go
@@ -1,63 +1,58 @@
 package connection
 
 import (
-	"context"
 	"strings"
-	"sync"
 
 	"github.com/pritunl/pritunl-client-electron/service/sprofile"
 	"github.com/sirupsen/logrus"
 )
 
 type Profile struct {
-	conn               *Connection        `json:"-"`
-	Id                 string             `json:"id"`
-	Mode               string             `json:"mode"`
-	OrgId              string             `json:"org_id"`
-	UserId             string             `json:"user_id"`
-	ServerId           string             `json:"server_id"`
-	SyncHosts          []string           `json:"sync_hosts"`
-	SyncToken          string             `json:"sync_token"`
-	SyncSecret         string             `json:"sync_secret"`
-	Data               string             `json:"data"`
-	Username           string             `json:"username"`
-	Password           string             `json:"password"`
-	DynamicFirewall    bool               `json:"dynamic_firewall"`
-	GeoSort            string             `json:"geo_sort"`
-	ForceConnect       bool               `json:"force_connect"`
-	DeviceAuth         bool               `json:"device_auth"`
-	DisableGateway     bool               `json:"disable_gateway"`
-	DisableDns         bool               `json:"disable_dns"`
-	RestrictClient     bool               `json:"restrict_client"`
-	ForceDns           bool               `json:"force_dns"`
-	SsoAuth            bool               `json:"sso_auth"`
-	ServerPublicKey    string             `json:"server_public_key"`
-	ServerBoxPublicKey string             `json:"server_box_public_key"`
-	TokenTtl           int                `json:"token_ttl"`
-	Reconnect          bool               `json:"reconnect"`
-	Timeout            bool               `json:"timeout"`
-	SystemInteractive  bool               `json:"-"`
-	SystemProfile      bool               `json:"-"`
-	requestCancelLock  sync.Mutex         `json:"-"`
-	requestCancel      context.CancelFunc `json:"-"`
+	conn               *Connection `json:"-"`
+	Id                 string      `json:"id"`
+	Mode               string      `json:"mode"`
+	OrgId              string      `json:"org_id"`
+	UserId             string      `json:"user_id"`
+	ServerId           string      `json:"server_id"`
+	SyncHosts          []string    `json:"sync_hosts"`
+	SyncToken          string      `json:"sync_token"`
+	SyncSecret         string      `json:"sync_secret"`
+	Data               string      `json:"data"`
+	Username           string      `json:"username"`
+	Password           string      `json:"password"`
+	DynamicFirewall    bool        `json:"dynamic_firewall"`
+	GeoSort            string      `json:"geo_sort"`
+	ForceConnect       bool        `json:"force_connect"`
+	DeviceAuth         bool        `json:"device_auth"`
+	DisableGateway     bool        `json:"disable_gateway"`
+	DisableDns         bool        `json:"disable_dns"`
+	RestrictClient     bool        `json:"restrict_client"`
+	ForceDns           bool        `json:"force_dns"`
+	SsoAuth            bool        `json:"sso_auth"`
+	ServerPublicKey    string      `json:"server_public_key"`
+	ServerBoxPublicKey string      `json:"server_box_public_key"`
+	RegistrationKey    string      `json:"registration_key"`
+	TokenTtl           int         `json:"token_ttl"`
+	Reconnect          bool        `json:"reconnect"`
+	Timeout            bool        `json:"timeout"`
+	SystemProfile      bool        `json:"-"`
 }
 
 func (p *Profile) Fields() logrus.Fields {
 	return logrus.Fields{
-		"profile_id":                 p.Id,
-		"profile_mode":               p.Mode,
-		"profile_dynamic_firewall":   p.DynamicFirewall,
-		"profile_device_auth":        p.DeviceAuth,
-		"profile_disable_gateway":    p.DisableGateway,
-		"profile_disable_dns":        p.DisableDns,
-		"profile_geo_sort":           p.IsGeoSort(),
-		"profile_force_connect":      p.ForceConnect,
-		"profile_force_dns":          p.ForceDns,
-		"profile_sso_auth":           p.SsoAuth,
-		"profile_reconnect":          p.Reconnect,
-		"profile_timeout":            p.Timeout,
-		"profile_system_profile":     p.SystemProfile,
-		"profile_system_interactive": p.SystemInteractive,
+		"profile_id":               p.Id,
+		"profile_mode":             p.Mode,
+		"profile_dynamic_firewall": p.DynamicFirewall,
+		"profile_device_auth":      p.DeviceAuth,
+		"profile_disable_gateway":  p.DisableGateway,
+		"profile_disable_dns":      p.DisableDns,
+		"profile_geo_sort":         p.IsGeoSort(),
+		"profile_force_connect":    p.ForceConnect,
+		"profile_force_dns":        p.ForceDns,
+		"profile_sso_auth":         p.SsoAuth,
+		"profile_reconnect":        p.Reconnect,
+		"profile_timeout":          p.Timeout,
+		"profile_system_profile":   p.SystemProfile,
 	}
 }
 
@@ -126,8 +121,8 @@ func (p *Profile) ImportSystemProfile(sprfl *sprofile.Sprofile) {
 	p.SsoAuth = sprfl.SsoAuth
 	p.ServerPublicKey = serverPublicKey
 	p.ServerBoxPublicKey = sprfl.ServerBoxPublicKey
+	p.RegistrationKey = sprfl.RegistrationKey
 	p.TokenTtl = sprfl.TokenTtl
 	p.Reconnect = true
-	p.SystemInteractive = sprfl.Interactive
 	p.SystemProfile = true
 }
