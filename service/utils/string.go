59bc98965a1d2e3a9f7e3473d837c9427f9bd49c
Zachary Huff <zach.huff.386@gmail.com>
2024-10-11 13:34:23 -0400

    Add filter str n to service utils

--- service/utils/string.go
+++ service/utils/string.go
@@ -11,3 +11,11 @@ var (
 func FilterStr(input string) string {
 	return string(alphaNumRe.ReplaceAll([]byte(input), []byte("")))
 }
+
+func FilterStrN(input string, n int) string {
+	str := string(alphaNumRe.ReplaceAll([]byte(input), []byte("")))
+	if len(str) > n {
+		str = str[:n]
+	}
+	return str
+}
