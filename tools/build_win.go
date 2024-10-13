b48a2076176c14a6b8f33330ff78877fd2c201bf
Zachary Huff <zach.huff.386@gmail.com>
2024-10-13 18:53:23 -0400

    Add arm to windows build

--- tools/build_win.go
+++ tools/build_win.go
@@ -50,6 +50,40 @@ func main() {
 		panic(err)
 	}
 
+	err = os.Rename("service.exe", "service_amd64.exe")
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("go", "build", "-v", "-ldflags", "-H windowsgui")
+	cmd.Env = append(os.Environ(), "GOARCH=arm64")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command(signtool,
+		"sign",
+		"/sha1", "055219bfb2cfe0b43a7e653da125b2f74bbe3003",
+		"/tr", "http://timestamp.sectigo.com",
+		"/td", "sha256",
+		"/fd", "sha256",
+		"service.exe",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Rename("service.exe", "service_arm64.exe")
+	if err != nil {
+		panic(err)
+	}
+
 	err = os.Chdir(filepath.Join("..", "cli"))
 	if err != nil {
 		panic(err)
@@ -67,6 +101,55 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command(signtool,
+		"sign",
+		"/sha1", "055219bfb2cfe0b43a7e653da125b2f74bbe3003",
+		"/tr", "http://timestamp.sectigo.com",
+		"/td", "sha256",
+		"/fd", "sha256",
+		"cli.exe",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Rename("cli.exe", "cli_amd64.exe")
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("go", "build", "-v")
+	cmd.Env = append(os.Environ(), "GOARCH=arm64")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command(signtool,
+		"sign",
+		"/sha1", "055219bfb2cfe0b43a7e653da125b2f74bbe3003",
+		"/tr", "http://timestamp.sectigo.com",
+		"/td", "sha256",
+		"/fd", "sha256",
+		"cli.exe",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Rename("cli.exe", "cli_arm64.exe")
 	if err != nil {
 		panic(err)
 	}
@@ -112,6 +195,105 @@ func main() {
 
 	err = os.Chdir(filepath.Join("..", "build", "win",
 		"pritunl-win32-x64"))
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("npx",
+		"@electron/fuses",
+		"write", "--app", "pritunl.exe",
+		"RunAsNode=off",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("npx",
+		"@electron/fuses",
+		"write", "--app", "pritunl.exe",
+		"EnableNodeOptionsEnvironmentVariable=off",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("npx",
+		"@electron/fuses",
+		"write", "--app", "pritunl.exe",
+		"EnableNodeCliInspectArguments=off",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("npx",
+		"@electron/fuses",
+		"write", "--app", "pritunl.exe",
+		"EnableEmbeddedAsarIntegrityValidation=on",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command("npx",
+		"@electron/fuses",
+		"write", "--app", "pritunl.exe",
+		"OnlyLoadAppFromAsar=on",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command(signtool,
+		"sign",
+		"/sha1", "055219bfb2cfe0b43a7e653da125b2f74bbe3003",
+		"/tr", "http://timestamp.sectigo.com",
+		"/td", "sha256",
+		"/fd", "sha256",
+		"pritunl.exe",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd = exec.Command(
+		".\\node_modules\\.bin\\electron-packager",
+		".\\",
+		"pritunl",
+		"--platform=win32",
+		"--arch=arm64",
+		"--icon=www\\img\\logo.ico",
+		"--out=..\\build\\win",
+		"--prune",
+		"--asar",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Chdir(filepath.Join("..", "build", "win",
+		"pritunl-win32-arm64"))
 	if err != nil {
 		panic(err)
 	}
