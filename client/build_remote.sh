b3dbddd002e822303bdeb0e36a6e0f07755827d0
Zachary Huff <zach.huff.386@gmail.com>
2025-07-14 12:00:32 -0400

    Update build remove in client

--- client/build_remote.sh
+++ client/build_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
+rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-client-electron/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
 
 ssh cloud@$NPM_SERVER "
 cd /home/cloud/pritunl-client-www/
@@ -9,15 +9,15 @@ rm -rf node_modules
 npm install
 "
 
-scp $NPM_SERVER:/home/cloud/pritunl-client-www/package.json /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/package.json
-scp $NPM_SERVER:/home/cloud/pritunl-client-www/package-lock.json /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/package-lock.json
-rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/node_modules/ /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/node_modules/
-rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
+scp $NPM_SERVER:/home/cloud/pritunl-client-www/package.json /home/cloud/git/pritunl-client-electron/client/package.json
+scp $NPM_SERVER:/home/cloud/pritunl-client-www/package-lock.json /home/cloud/git/pritunl-client-electron/client/package-lock.json
+rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/node_modules/ /home/cloud/git/pritunl-client-electron/client/node_modules/
+rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-client-electron/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
 
 ssh cloud@$NPM_SERVER "
 cd /home/cloud/pritunl-client-www/
 sh build.sh
 "
 
-rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/dist/ /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/dist/
-rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/dist-dev/ /home/cloud/go/src/github.com/pritunl/pritunl-client-electron/client/dist-dev/
+rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/dist/ /home/cloud/git/pritunl-client-electron/client/dist/
+rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/dist-dev/ /home/cloud/git/pritunl-client-electron/client/dist-dev/
