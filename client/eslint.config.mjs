5d162d0cc583ddebd62cb5f9c6d3b5e331bd45cf
C-A de Salaberry <ca.s@padoa.fr>
2025-09-19 10:34:20 +0200

    chore(deps): :truck: migrate to prettier vanilla

---
+++ client/eslint.config.mjs
@@ -0,0 +1,29 @@
+// eslint.config.mjs
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**", "dist-dev/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+
+    languageOptions: {
+      parser: tsparser,
+      sourceType: "module",
+    },
+
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+    },
+  },
+];
